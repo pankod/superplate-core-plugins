ccb99d97cc75d2db92ff83a01b0dcd0e73974850
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-01-13 15:38:39 +0300

    cleanup

--- plugins/redux/extend.js
+++ plugins/redux/extend.js
@@ -5,34 +5,13 @@ const base = {
       "import { Provider } from 'react-redux'",
       "import store from '../src/redux/store'",
     ],
-    inner: [
-      `
-
-    `,
-    ],
+    inner: [``],
     wrapper: ["<>", "<Provider store={store}>", "</Provider>", "</>"],
   },
 };
 
-/**
- * wrapper arrayi cift sayi uzunlugunda olmali =)
- * yoksa bir error throw yapip bu durumun hangi pluginde olustugunu ekrana basalim.
- * Template gibi kullanmak yerine dizi kullanmak daha anlasilir olur gibi dusundum
- * daha sonrasinda da extend edebilmek adina.
- */
-
 module.exports = {
   extend(answers) {
-    /**
-     * if there are any conditions depending on other answers
-     * handle them in this function and return the derived version of the base object.
-     */
-    const hasPrettier = answers.includes("prettier");
-    if (hasPrettier) {
-      base._app.inner.push(`
-        console.log("prettierla tailwindin ne alakasi var simdi?");
-        `);
-    }
     return base;
   },
 };

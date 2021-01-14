0ad652d4eef9d0d0055af4405e2533bd60ba3f76
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-01-14 12:13:31 +0300

    Add counter styles.

--- plugins/redux/src/components/Counter/index.tsx
+++ plugins/redux/src/components/Counter/index.tsx
@@ -3,21 +3,40 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { increase, decrease } from "@redux/actions";
 import { IState } from "@redux/istate";
+import styles from "./index.module.css";
 
 export default function Counter() {
   const dispatch = useDispatch();
   const count = useSelector((state: IState) => state.counter.count);
 
   return (
-    <>
-      <h2>Counter</h2>
-      <button type="button" onClick={() => dispatch(increase())}>
-        +
-      </button>
-      <strong style={{ margin: 10 }}>{count}</strong>
-      <button type="button" onClick={() => dispatch(decrease())}>
-        -
-      </button>
-    </>
+    <div className={styles["box"]}>
+      <div>
+        <h2>Counter</h2>
+        <button
+          className={[styles.button, styles["button--increase"]].join(" ")}
+          type="button"
+          onClick={() => dispatch(increase())}
+        >
+          +
+        </button>
+        <span className={styles["count"]}>{count}</span>
+        <button
+          className={[styles.button, styles["button--decrease"]].join(" ")}
+          type="button"
+          onClick={() => dispatch(decrease())}
+        >
+          -
+        </button>
+      </div>
+
+      <a
+        className={styles["link"]}
+        href="https://react-redux.js.org/"
+        target="_blank"
+      >
+        Go to redux documentation.
+      </a>
+    </div>
   );
 }

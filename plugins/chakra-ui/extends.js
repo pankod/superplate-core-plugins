ad1ae3982faebff96c8fe0f45ae74f43f1636ac6
Ali Emir Sen <senaliemir@gmail.com>
2021-01-14 13:16:05 +0300

    update chakra path alias

--- plugins/chakra-ui/extends.js
+++ plugins/chakra-ui/extends.js
@@ -2,7 +2,7 @@ const base = {
     _app: {
         import: [
             'import { ChakraProvider } from "@chakra-ui/react";',
-            'import theme from "@chakra/theme";'
+            'import theme from "@definitions/chakra/theme";'
         ],
         inner: [],
         wrapper: [

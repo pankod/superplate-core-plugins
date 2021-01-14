0bc9e79be45c3929d2fefdbf3ef541a806940c24
Ali Emir Şen <senaliemir@gmail.com>
2021-01-14 13:23:13 +0300

    Chakra plugin (#38)

--- prompt.js
+++ prompt.js
@@ -1,68 +1,69 @@
 module.exports = {
-  prompts: [
-    {
-      name: "ui",
-      message: "UI framework:",
-      type: "select",
-      pageSize: 3,
-      choices: [
-        { message: "None", name: "none" },
-        { message: "Tailwind CSS", name: "tailwind" },
+      prompts: [
+        {
+          name: "ui",
+          message: "UI framework:",
+          type: "select",
+          pageSize: 3,
+          choices: [
+            { message: "None", name: "none" },
+            { message: "Tailwind CSS", name: "tailwind" },
+            { message: "Chakra UI", name: "chakra-ui" }
+          ],
+          default: "none",
+        },
+        {
+          name: "css_features",
+          message: "CSS Solution:",
+          type: "select",
+          pageSize: 2,
+          choices: [{ message: "SASS/SCSS", name: "sass" }],
+          default: "none",
+        },
+        {
+          name: "features",
+          message: "Features:",
+          type: "multiselect",
+          pageSize: 2,
+          choices: [
+            { message: "Axios", name: "axios" },
+            { message: "Prettier", name: "prettier" },
+            { message: "Jest", name: "jest" },
+          ],
+          default: "none",
+        },
+        {
+          name: "docker",
+          message: "Docker integration:",
+          type: "select",
+          pageSize: 3,
+          choices: [
+            { message: "None", name: "none" },
+            { message: "Dockerfile", name: "Docker" },
+          ],
+          default: "none",
+        },
+        {
+          name: "CI",
+          message: "Continuous integration:",
+          type: "select",
+          choices: [
+            { message: "None", name: "none" },
+            { message: "GitHub Actions", name: "github-actions" },
+            { message: "Travis", name: "travis" },
+          ],
+          default: "none",
+        },
+        {
+          name: "state-management",
+          message: "State Management:",
+          type: "select",
+          pageSize: 3,
+          choices: [
+            { message: "None", name: "none" },
+            { message: "Redux", name: "redux" },
+          ],
+          default: "none",
+        },
       ],
-      default: "none",
-    },
-    {
-      name: "css_features",
-      message: "CSS Solution:",
-      type: "select",
-      pageSize: 2,
-      choices: [{ message: "SASS/SCSS", name: "sass" }],
-      default: "none",
-    },
-    {
-      name: "features",
-      message: "Features:",
-      type: "multiselect",
-      pageSize: 2,
-      choices: [
-        { message: "Axios", name: "axios" },
-        { message: "Prettier", name: "prettier" },
-        { message: "Jest", name: "jest" },
-      ],
-      default: "none",
-    },
-    {
-      name: "docker",
-      message: "Docker integration:",
-      type: "select",
-      pageSize: 3,
-      choices: [
-        { message: "None", name: "none" },
-        { message: "Dockerfile", name: "Docker" },
-      ],
-      default: "none",
-    },
-    {
-      name: "CI",
-      message: "Continuous integration:",
-      type: "select",
-      choices: [
-        { message: "None", name: "none" },
-        { message: "GitHub Actions", name: "github-actions" },
-        { message: "Travis", name: "travis" },
-      ],
-      default: "none",
-    },
-    {
-      name: "state-management",
-      message: "State Management:",
-      type: "select",
-      pageSize: 3,
-      choices: [
-        { message: "None", name: "none" },
-        { message: "Redux", name: "redux" },
-      ],
-      default: "none",
-    },
-  ],
-};
+    };

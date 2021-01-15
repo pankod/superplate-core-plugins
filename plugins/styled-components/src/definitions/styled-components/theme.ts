508072ccbfd4d351cf6db3fe7d395f22b9f9e984
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-01-15 16:57:22 +0300

    create definitions for theme

---
+++ plugins/styled-components/src/definitions/styled-components/theme.ts
@@ -0,0 +1,12 @@
+// #region Global Imports
+import { DefaultTheme } from "styled-components";
+// #endregion Global Imports
+
+const theme: DefaultTheme = {
+    colors: {
+        transparent: "transparent",
+        red: "#ff2a44",
+    },
+};
+
+export { theme };

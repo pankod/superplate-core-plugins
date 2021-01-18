f71be3e0ed6c296c1f28c6511bb4d298554257c6
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-01-18 12:06:51 +0300

    Add index file.

---
+++ plugins/bootstrap/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Header, Banner, Footer, Cards } from "@components";
+
+const Home: React.FC = () => {
+    return (
+        <>
+            <Header />
+            <Banner />
+            <Cards />
+            <Footer />
+        </>
+    );
+};
+
+export default Home;

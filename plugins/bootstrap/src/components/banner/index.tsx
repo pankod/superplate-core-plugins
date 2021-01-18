362dcf92fb6c8a9950d533e2a3aec778a1a69da3
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-01-18 14:26:27 +0300

    Move scss files.

--- plugins/bootstrap/src/components/banner/index.tsx
+++ plugins/bootstrap/src/components/banner/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import cx from "classnames";
 
 import { Container, Button } from "@components";
-import styles from "./index.module.scss";
+import styles from "./index.module.<%= css_features %>";
 
 export const Banner: React.FC = () => {
     return (

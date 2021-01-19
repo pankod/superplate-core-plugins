cc5f3944476e03bc6f8fda8cfd4c6d2de855fae7
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-01-19 15:08:59 +0300

    Fixed antd css path for stroybook.

--- plugins/storybook/.storybook/preview.js
+++ plugins/storybook/.storybook/preview.js
@@ -3,7 +3,7 @@ import styles from "../src/styles/app.scss";
 <%_ } _%>
 
 <%_ if (ui.includes("antd")) { _%>
-import styles from "../src/styles/antd.less";
+import "antd/dist/antd.css";
 <%_ } _%>
 
 export const parameters = {

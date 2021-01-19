0e156298a84e8a0bbd5db98cae381e21bf5f24c2
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-01-19 11:22:27 +0300

    update styled.ts

--- plugins/styled-system/src/components/styled-system/styled.ts
+++ plugins/styled-system/src/components/styled-system/styled.ts
@@ -13,17 +13,18 @@ export const CardHeader = styled.header`
   h2 {
     font-weight: 600;
   }
+
   ${border}
   ${space}
 `;
 
 export const CardContent = styled.main`
-  ${space}
-
   p {
     margin-bottom: 1rem;
     color: darkslategrey;
   }
+  
+  ${space}
 `;
 
 export const CardFooter = styled.footer`

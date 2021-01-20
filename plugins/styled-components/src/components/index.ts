e853f1c75a1ade3c828c5943cc8bf1b5865e1c08
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-01-20 21:40:08 +0300

    exports comoponents

---
+++ plugins/styled-components/src/components/index.ts
@@ -0,0 +1,7 @@
+export { Header } from "./header";
+export { Banner } from "./banner";
+export { Logo } from "./logo";
+export { Main } from "./main";
+export { Button } from "./button";
+export { Cards } from "./cards";
+export { Footer } from "./footer";

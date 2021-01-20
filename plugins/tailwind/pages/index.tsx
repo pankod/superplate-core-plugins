00a46cd5a716158cf61d9388be02b5089024084a
Ali Emir Sen <senaliemir@gmail.com>
2021-01-20 17:18:14 +0300

    add tailwind landing page

---
+++ plugins/tailwind/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Header, Main, Footer, Cards } from "@components";
+
+const Home: React.FC = () => {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Cards />
+            <Footer />
+        </>
+    );
+};
+
+export default Home;

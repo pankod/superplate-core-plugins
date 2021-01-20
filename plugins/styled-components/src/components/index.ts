f36100ead83702de232d56430d301fecf66a4070
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-01-20 21:44:39 +0300

    delete main export

--- plugins/styled-components/src/components/index.ts
+++ plugins/styled-components/src/components/index.ts
@@ -1,7 +1,6 @@
 export { Header } from "./header";
 export { Banner } from "./banner";
 export { Logo } from "./logo";
-export { Main } from "./main";
 export { Button } from "./button";
 export { Cards } from "./cards";
 export { Footer } from "./footer";

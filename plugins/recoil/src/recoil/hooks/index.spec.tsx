a9b7afa7d087a9050ea1841fabf94ef08edb0e71
Ali Emir Sen <senaliemir@gmail.com>
2021-01-20 15:08:36 +0300

    add testing-library tests for recoil and add ejs check

--- plugins/recoil/src/recoil/hooks/index.spec.tsx
+++ plugins/recoil/src/recoil/hooks/index.spec.tsx
@@ -1,5 +1,15 @@
-import mount from "@test/mount";
+<%_ if (testing === 'enzyme') { _%>
 import { act } from "react-dom/test-utils";
+<%_ } else if (testing === 'testing-library') { _%>
+import { renderHook, act } from "@testing-library/react-hooks";
+<%_ } _%>
+
+<%_ if (testing === 'enzyme') { _%>
+import mount from "@test/mount";
+<%_ } else if (testing === 'testing-library') { _%>
+import { AllTheProviders } from "@test";
+<%_ } _%>
+
 import { useCounter } from "./useCounter";
 
 /**
@@ -7,6 +17,8 @@ import { useCounter } from "./useCounter";
  * You can test your custom recoil hooks and their behaviors,
  * also you can create snapshot tests for your selectors.
  */
+
+<%_ if (testing === 'enzyme') { _%>
 
 const HookWrapper = ({ hook }) => {
     const val = hook();
@@ -56,3 +68,48 @@ describe("Recoil useCounter Hook", () => {
         expect(count).toEqual(0);
     });
 });
+
+<%_ } else if (testing === 'testing-library') { _%>
+
+describe("Recoil useCounter Hook", () => {
+  it("should set initial value to 0", async () => {
+    const { result } = renderHook(() => useCounter(), {
+      wrapper: AllTheProviders,
+    });
+    const [count] = result.current;
+
+    expect(count).toEqual(0);
+  });
+
+  it("should increment counter by 1", async () => {
+    const { result } = renderHook(() => useCounter(), {
+      wrapper: AllTheProviders,
+    });
+    const [_, { increase }] = result.current;
+
+    act(() => {
+      increase();
+    });
+
+    const [count] = result.current;
+
+    expect(count).toBe(1);
+  });
+
+  it("should decrement counter by 1", async () => {
+    const { result } = renderHook(() => useCounter(), {
+      wrapper: AllTheProviders,
+    });
+    const [_, { decrease }] = result.current;
+
+    act(() => {
+      decrease();
+    });
+
+    const [count] = result.current;
+
+    expect(count).toBe(-1);
+  });
+});  
+
+<%_ } _%>

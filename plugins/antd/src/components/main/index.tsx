b072762c7cd9762afb37842547e280d337e83334
Muharrem <mkocadere@gmail.com>
2021-01-21 15:13:17 +0300

    fix mistake: revert antd main component

--- plugins/antd/src/components/main/index.tsx
+++ plugins/antd/src/components/main/index.tsx
@@ -1,20 +1,26 @@
 import React from "react";
-import { Box, useTheme } from "@chakra-ui/react";
 
 import { Button } from "@components";
 
 export const Main: React.FC = () => {
-    const theme = useTheme();
     return (
-        <Box bg="main.100" color="white" textAlign="center" py={10}>
-            <h1 style={{ fontSize: theme.fontSizes["5xl"] }}>next-cli</h1>
-            <p style={{ fontSize: theme.fontSizes["lg"] }}>
+        <div
+            style={{
+                backgroundColor: "#282c34",
+                color: "#fff",
+                textAlign: "center",
+                paddingTop: 32,
+                paddingBottom: 32,
+            }}
+        >
+            <h1 style={{ color: "#fff", fontSize: 46 }}>next-cli</h1>
+            <p style={{ fontSize: 18 }}>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry.
             </p>
-            <Button colorScheme="blue" variant="solid" size="md">
+            <Button type="primary" size="large">
                 Docs
             </Button>
-        </Box>
+        </div>
     );
 };

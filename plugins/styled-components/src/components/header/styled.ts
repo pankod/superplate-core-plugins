66d56f519e9294527bc0ecc0dbaab945eb487461
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-01-21 11:30:22 +0300

    update header component

--- plugins/styled-components/src/components/header/styled.ts
+++ plugins/styled-components/src/components/header/styled.ts
@@ -1,37 +1,6 @@
 import styled from "styled-components";
 
-export const HeaderWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    background-color: ${({ theme }) => theme.colors.blackGrey};
-    padding: 0 150px;
-`;
-
 export const Container = styled.div`
-    max-width: 1140px;
-    min-width: 1140px;
-`;
-
-export const Nav = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-`;
-
-export const NavBrand = styled.div`
-    display: flex;
-    align-items: center;
-`;
-
-export const Menu = styled.div`
-    margin-left: 50px;
-`;
-
-export const MenuItem = styled.a`
-    text-decoration: none;
-    color: ${({ theme }) => theme.colors.white};
-
-    &:hover {
-        text-decoration: underline;
-    }
+    text-align: center;
+    background-color: ${({ theme }) => theme.colors.blackGrey};
 `;

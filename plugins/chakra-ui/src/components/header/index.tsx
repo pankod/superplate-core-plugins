82e5e70449e380e4a3372be5e53ec3653288b48d
Muharrem <mkocadere@gmail.com>
2021-01-21 15:40:09 +0300

    fix h

--- plugins/chakra-ui/src/components/header/index.tsx
+++ plugins/chakra-ui/src/components/header/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
 
 import { Logo } from "@components";
 
 export const Header: React.FC = () => {
     return (
-        <Box bg="header.100" textAlign="center">
+        <Center bg="header.100">
             <Logo />
-        </Box>
+        </Center>
     );
 };

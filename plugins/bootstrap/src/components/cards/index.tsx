afc96dd93f56365081fae5fd6ed0db0891aebf7b
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-01-22 15:54:50 +0300

    Resolves #128

--- plugins/bootstrap/src/components/cards/index.tsx
+++ plugins/bootstrap/src/components/cards/index.tsx
@@ -1,74 +1,24 @@
 import React from "react";
 import { Row, Col, Container, Card } from "react-bootstrap";
 
-export const Cards: React.FC = () => {
-    const data: { title: string; content: string }[] = [
-        {
-            title: "Redux",
-            content:
-                "React Redux is the official React binding for Redux. It lets your React components read data from a Redux store, and dispatch actions to the store to update data.",
-        },
-        {
-            title: "Styled-Components",
-            content:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-        },
-        {
-            title: "Bootstrap",
-            content:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-        },
-        {
-            title: "SVGR",
-            content:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-        },
-        {
-            title: "Styled-Components",
-            content:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-        },
-        {
-            title: "Bootstrap",
-            content:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-        },
-        {
-            title: "SVGR",
-            content:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-        },
-        {
-            title: "Styled-Components",
-            content:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-        },
-        {
-            title: "Bootstrap",
-            content:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-        },
-        {
-            title: "SVGR",
-            content:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-        },
-    ];
+import data from "@public/meta.json";
 
-    return (
-        <Container className="my-5">
-            <Row>
-                {data.map((item) => (
-                    <Col md={4} key={item.title} className="mb-3">
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>{item.title}</Card.Title>
-                                <Card.Text>{item.content}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
-        </Container>
-    );
+export const Cards: React.FC = () => {
+
+  return (
+    <Container className="my-5">
+      <Row>
+        {(data?.plugins ?? []).map((plugin) => (
+          <Col md={4} key={plugin.name} className="mb-3">
+            <Card className="border-none">
+              <Card.Body>
+                <Card.Title>{plugin.name}</Card.Title>
+                <Card.Text>{plugin.description}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 };

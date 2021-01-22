0e8caa1e83e3102cd14a7f451ddfae082de1a3e8
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-01-22 16:26:59 +0300

    Resolves #140

--- plugins/chakra-ui/src/components/cards/index.tsx
+++ plugins/chakra-ui/src/components/cards/index.tsx
@@ -5,16 +5,16 @@ import data from "@public/meta.json";
 
 export const Cards: React.FC = () => {
 
-    return (
-        <SimpleGrid columns={4} spacing={10} px={20} py={10}>
-            {(data?.plugins ?? []).map((plugin) => (
-                <Box>
-                    <Heading fontSize={16} fontWeight={500} py={5}>
-                        {plugin.name}
-                    </Heading>
-                    <Text fontSize={14}>{plugin.description}</Text>
-                </Box>
-            ))}
-        </SimpleGrid>
-    );
+  return (
+    <SimpleGrid columns={4} spacing={10} px={20} py={10}>
+      {(data?.plugins ?? []).map((plugin) => (
+        <Box key={plugin.name}>
+          <Heading fontSize={16} fontWeight={500} py={5}>
+            {plugin.name}
+          </Heading>
+          <Text fontSize={14}>{plugin.description}</Text>
+        </Box>
+      ))}
+    </SimpleGrid>
+  );
 };

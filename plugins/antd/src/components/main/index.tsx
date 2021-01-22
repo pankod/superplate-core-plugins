5abd63d13c3d97a1aba5a7376dfe19d34808345d
Muharrem <mkocadere@gmail.com>
2021-01-22 12:22:43 +0300

    update antd, bootstrap: add data-test attributes. add anchor to docs page

--- plugins/antd/src/components/main/index.tsx
+++ plugins/antd/src/components/main/index.tsx
@@ -13,13 +13,24 @@ export const Main: React.FC = () => {
                 paddingBottom: 32,
             }}
         >
-            <h1 style={{ color: "#fff", fontSize: 46 }}>electio</h1>
+            <h1
+                data-test="main-heading"
+                style={{ color: "#fff", fontSize: 46 }}
+            >
+                electio
+            </h1>
             <p style={{ fontSize: 18 }}>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry.
             </p>
             <Button type="primary" size="large">
-                Docs
+                <a
+                    data-test="docs-btn-anchor"
+                    href="https://pankod.github.io/electio/"
+                    target="_blank"
+                >
+                    Docs
+                </a>
             </Button>
         </div>
     );

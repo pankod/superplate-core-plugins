3f27ba7707366c0a8afc9492eb0247c80721f771
necatiozmen <necatiozmn@gmail.com>
2021-01-22 15:43:24 +0300

    Lint scss button

--- plugins/scss/src/components/scss/button/index.stories.tsx
+++ plugins/scss/src/components/scss/button/index.stories.tsx
@@ -4,22 +4,14 @@ import { withKnobs, select } from "@storybook/addon-knobs";
 import { Button as ScssButton } from "./index";
 
 export default {
-  title: "Button SCSS",
-  component: ScssButton,
-  decorators: [withKnobs],
+    title: "Button SCSS",
+    component: ScssButton,
+    decorators: [withKnobs],
 };
 
 export const Basic = () => <ScssButton>Submit</ScssButton>;
 
 export const DynamicVariables = () => {
-
-  const label = select(
-    "label",
-    [
-     "Submit",
-     "Cancel"
-    ],
-    "Docs"
-  );
-  return <ScssButton>{label}</ScssButton>;
+    const label = select("label", ["Submit", "Cancel"], "Docs");
+    return <ScssButton>{label}</ScssButton>;
 };

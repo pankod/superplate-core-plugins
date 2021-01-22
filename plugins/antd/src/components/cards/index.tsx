6d99a0c4e14c5828f7f892b93219e3550d243c4c
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-01-22 16:06:29 +0300

    Resolves #129

--- plugins/antd/src/components/cards/index.tsx
+++ plugins/antd/src/components/cards/index.tsx
@@ -2,66 +2,15 @@ import React from "react";
 import { Row, Col } from "antd";
 
 import { Card } from "@components";
+import data from "@public/meta.json";
 
 export const Cards: React.FC = () => {
-  const data: { title: string; content: string }[] = [
-    {
-      title: "Redux",
-      content:
-        "React Redux is the official React binding for Redux. It lets your React components read data from a Redux store, and dispatch actions to the store to update data.",
-    },
-    {
-      title: "Styled-Components",
-      content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-    },
-    {
-      title: "Bootstrap",
-      content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-    },
-    {
-      title: "SVGR",
-      content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-    },
-    {
-      title: "Styled-Components",
-      content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-    },
-    {
-      title: "Bootstrap",
-      content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-    },
-    {
-      title: "SVGR",
-      content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-    },
-    {
-      title: "Styled-Components",
-      content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-    },
-    {
-      title: "Bootstrap",
-      content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-    },
-    {
-      title: "SVGR",
-      content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.",
-    },
-  ];
 
   return (
     <Row>
-      {data.map((item) => (
-        <Col md={6} key={item.title}>
-          <Card title={item.title}>{item.content}</Card>
+      {(data?.plugins ?? []).map((plugin) => (
+        <Col md={6} key={plugin.name}>
+          <Card title={plugin.name}>{plugin.description}</Card>
         </Col>
       ))}
     </Row>

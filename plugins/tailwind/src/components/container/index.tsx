52907ab317306fa679291b0fb1ba4afa91074916
Ali Emir Şen <senaliemir@gmail.com>
2021-01-22 13:23:43 +0300

    Tailwind footer (#136)
    * add container element
    
    * fill screen with cards content

---
+++ plugins/tailwind/src/components/container/index.tsx
@@ -0,0 +1,3 @@
+export const Container = ({ children }) => {
+    return <div className="min-h-screen flex flex-col">{children}</div>;
+};
--- plugins/tailwind/src/components/index.ts
+++ plugins/tailwind/src/components/index.ts
@@ -4,3 +4,4 @@ export { Main } from "./main";
 export { Button } from "./button";
 export { Cards } from "./cards";
 export { Footer } from "./footer";
+export { Container } from "./container";

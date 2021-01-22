f770fd84dc869c7bb25e98983a17cb710930de24
Ali Emir Sen <senaliemir@gmail.com>
2021-01-22 12:56:54 +0300

    fill screen with cards content

--- plugins/tailwind/src/components/cards/index.tsx
+++ plugins/tailwind/src/components/cards/index.tsx
@@ -4,7 +4,7 @@ import data from "@public/meta.json";
 
 export const Cards: React.FC = () => {
     return (
-        <div className="container my-8 max-w-screen-lg mx-auto p-5">
+        <div className="flex-1 container my-8 max-w-screen-lg mx-auto p-5">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {(data?.plugins ?? []).map((plugin) => (
                     <div

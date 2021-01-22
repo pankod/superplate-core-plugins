cea72240e2ae54a150951a92fe25978a0f7d1f6f
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-01-22 16:15:51 +0300

    Resolves #132

--- plugins/antd/src/components/cards/index.tsx
+++ plugins/antd/src/components/cards/index.tsx
@@ -7,7 +7,7 @@ import data from "@public/meta.json";
 export const Cards: React.FC = () => {
 
   return (
-    <Row>
+    <Row style={{ flex: 1}}>
       {(data?.plugins ?? []).map((plugin) => (
         <Col md={6} key={plugin.name}>
           <Card title={plugin.name}>{plugin.description}</Card>

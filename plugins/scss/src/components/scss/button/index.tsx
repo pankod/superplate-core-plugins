d23a685742b75c7b304d052ef59467cb7ae58456
necatiozmen <necatiozmn@gmail.com>
2021-01-22 12:56:23 +0300

    Refactor button

--- plugins/scss/src/components/scss/button/index.tsx
+++ plugins/scss/src/components/scss/button/index.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
-export const Button: React.FC = ({ children }) => {
-    return <button>{children}</button>;
+export const Button: React.FC<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>
+> = ({ children, ...props }) => {
+    return (
+        <a
+            {...props}
+            target="_blank"
+            href="https://pankod.github.io/electio/"
+            rel="noopener noreferrer"
+            className="button"
+        >
+            {children}
+        </a>
+    );
 };

ed7da09c9e228ba4df6205d2fe8ac47fca66d87a
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-01-22 12:35:25 +0300

    fixed footer container to bottom

--- plugins/styled-components/src/components/footer/styled.ts
+++ plugins/styled-components/src/components/footer/styled.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    position: absolute;
+    width: 100%;
+    bottom: 0;
     text-align: center;
     padding: 2rem 0;
     background-color: ${({ theme }) => theme.colors.darkGrey};

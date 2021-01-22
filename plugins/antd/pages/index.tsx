a23f9a3c2005243752036e1e68692065012d30b9
burcukaragozzz <33669461+burcukaragozzz@users.noreply.github.com>
2021-01-22 16:23:50 +0300

    antd meta data and footer (#143)
    * Resolves #129
    
    * Resolves #132

--- plugins/antd/pages/index.tsx
+++ plugins/antd/pages/index.tsx
@@ -4,12 +4,12 @@ import { Header, Main, Cards, Footer } from "@components";
 
 const Home: React.FC = () => {
     return (
-        <>
+        <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh"}}>
             <Header />
             <Main />
             <Cards />
             <Footer />
-        </>
+        </div>
     );
 };
 

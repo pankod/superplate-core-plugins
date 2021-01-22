82aee4de10029b3c510603b36f5f2c5539ea9c32
Muharrem <mkocadere@gmail.com>
2021-01-22 11:54:58 +0300

    add cypress tests

---
+++ plugins/cypress/cypress/support/commands.ts
@@ -0,0 +1,7 @@
+Cypress.Commands.add("getBySel", (selector, ...args) => {
+    return cy.get(`[data-test=${selector}]`, ...args);
+});
+
+Cypress.Commands.add("getBySelLike", (selector, ...args) => {
+    return cy.get(`[data-test*=${selector}]`, ...args);
+});

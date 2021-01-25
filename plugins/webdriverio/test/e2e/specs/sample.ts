e27d40238ea0afe864408901d918312984e97952
Muharrem <mkocadere@gmail.com>
2021-01-25 17:27:09 +0300

    remove sample tests

--- plugins/webdriverio/test/e2e/specs/sample.ts
+++
@@ -1,6 +0,0 @@
-describe("My First Test", () => {
-    it("Visits the next-cli GitHub Page", () => {
-        browser.url("https://github.com/pankod/next-cli");
-        expect(browser).toHaveTitle("GitHub - pankod/next-cli");
-    });
-});

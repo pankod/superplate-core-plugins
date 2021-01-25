da71c6a68428b0f4dbcefea852c9628351f60798
Necati Özmen <necati@pankod.com>
2021-01-25 11:04:28 +0300

    Add scss plugin (#121)
    * Add scss plugin files
    
    * Add footer & logo component
    
    * Add scss path
    
    * Fix footer path
    
    * No verify
    
    * Update for meta json
    
    * Update props name
    
    * Refactor button
    
    * Add button storybook
    
    * Edit storybook config
    
    * Lint scss button
    
    * Add card storybook
    
    * Update plugins/scss/extend.js
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>
    
    * Add missing import
    
    * Change global style name
    
    * Refactor footer for linting
    
    * Refactor footer
    
    * Add key to card
    
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

--- plugins/storybook/.storybook/main.js
+++ plugins/storybook/.storybook/main.js
@@ -22,7 +22,7 @@ module.exports = {
                 {
                     loader: "css-loader",
                     options: {
-                        modules: false,
+                        modules: true,
                     },
                 },
                 "sass-loader",

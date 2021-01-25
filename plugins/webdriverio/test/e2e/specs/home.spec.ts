e88928b1f36c043ed5b8229e425a7d3f98cffb2d
Muharrem <mkocadere@gmail.com>
2021-01-25 10:53:03 +0300

    add beforeEach. add href test

--- plugins/webdriverio/test/e2e/specs/home.spec.ts
+++ plugins/webdriverio/test/e2e/specs/home.spec.ts
@@ -1,7 +1,17 @@
 describe("Homepage", () => {
+    beforeEach(() => {
+        browser.url("/")
+    })
     it("Brings header", async () => {
         browser.url("/");
         const heading = await $('[data-test="main-heading"]');
         expect(await heading.getText()).toBe("electio");
+    });
+
+    it("Should have true href", async () => {
+        const button = await $('[data-test="docs-btn-anchor"]')
+        const buttonHref = await button.getAttribute("href")
+        expect(buttonHref).toBeTruthy()
+        expect(buttonHref).toBe("https://pankod.github.io/electio/")
     });
 });

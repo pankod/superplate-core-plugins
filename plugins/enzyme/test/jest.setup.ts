5f3e76ac2055a1ef19e8c9aa418fe61b3bdf08b2
Ali Emir Şen <senaliemir@gmail.com>
2021-01-26 14:26:52 +0300

    dotenv for testing (#155)
    * add dotenv if testing !== none
    
    * add dotenv config to jest setup
    
    * remove dotenv from env plugin
    
    * add dotenv to testing plugins

--- plugins/enzyme/test/jest.setup.ts
+++ plugins/enzyme/test/jest.setup.ts
@@ -1,11 +1,14 @@
 import "isomorphic-unfetch";
 import nock from "nock";
+import dotenv from "dotenv";
 import Enzyme from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 <%_ if (features.indexOf("axios") >= 0) { _%>
 import httpAdapter from "axios/lib/adapters/http";
 import axios from "axios";
 <%_ } _%>
+
+dotenv.config({ path: ".env.test" });
 
 <%_ if (features.indexOf("axios") >= 0) { _%>
 axios.defaults.adapter = httpAdapter;
--- plugins/testing-library/test/jest.setup.ts
+++ plugins/testing-library/test/jest.setup.ts
@@ -1,9 +1,12 @@
 import "isomorphic-unfetch";
 import nock from "nock";
+import dotenv from "dotenv";
 <%_ if (features.indexOf("axios") >= 0) { _%>
 import httpAdapter from "axios/lib/adapters/http";
 import axios from "axios";
 <%_ } _%>
+
+dotenv.config({ path: ".env.test" });
 
 <%_ if (features.indexOf("axios") >= 0) { _%>
 axios.defaults.adapter = httpAdapter;

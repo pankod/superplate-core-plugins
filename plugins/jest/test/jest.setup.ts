653d5761f000ecfe0c3b233e3901edbd755b3b8f
Ali Emir Sen <senaliemir@gmail.com>
2021-01-26 12:02:35 +0300

    add dotenv to testing plugins

--- plugins/jest/test/jest.setup.ts
+++ plugins/jest/test/jest.setup.ts
@@ -1,16 +1,12 @@
 import "isomorphic-unfetch";
 import nock from "nock";
+import dotenv from "dotenv";
 <%_ if (features.indexOf("axios") >= 0) { _%>
 import httpAdapter from "axios/lib/adapters/http";
 import axios from "axios";
 <%_ } _%>
-<%_ if (features.indexOf("env") >= 0) { _%>
-import dotenv from "dotenv";
-<%_ } _%>
 
-<%_ if (features.indexOf("env") >= 0) { _%>
 dotenv.config({ path: ".env.test" });
-<%_ } _%>
 
 <%_ if (features.indexOf("axios") >= 0) { _%>
 axios.defaults.adapter = httpAdapter;

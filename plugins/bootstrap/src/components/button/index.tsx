6bc86d3dfd8decbafa3e7266cb133f30f16b07b7
burcukaragozzz <33669461+burcukaragozzz@users.noreply.github.com>
2021-01-27 11:48:43 +0300

    add unit tests for bootstrap (#165)

--- plugins/bootstrap/src/components/button/index.tsx
+++ plugins/bootstrap/src/components/button/index.tsx
@@ -4,5 +4,12 @@ import { Button as BaseButton, ButtonProps } from "react-bootstrap";
 export type IButton = ButtonProps;
 
 export const Button: React.FC<IButton> = ({ ...rest }) => {
-    return <BaseButton {...rest} />;
+    return (
+        <BaseButton
+            {...rest}
+            <%_ if (testing === 'testing-library') { _%>
+            data-testid="btn" 
+            <%_ } _%>
+        />
+    );
 };
--- plugins/bootstrap/src/components/cards/index.tsx
+++ plugins/bootstrap/src/components/cards/index.tsx
@@ -9,7 +9,7 @@ export const Cards: React.FC = () => {
     <Container className="my-5 flex-grow-1">
       <Row>
         {(data?.plugins ?? []).map((plugin) => (
-          <Col md={4} key={plugin.name} className="mb-3">
+          <Col md={4} key={plugin.name} className="mb-3" <% if (testing === 'testing-library') { %> data-testid="container" <% } %> >
             <Card className="border-none">
               <Card.Body>
                 <Card.Title>{plugin.name}</Card.Title>

a5c7393f0fe2b7aa6716d42c374a7736ab1fda8a
necatiozmen <necatiozmn@gmail.com>
2021-07-14 11:53:49 +0300

    Add zustand template

---
+++ plugins/zustand/src/components/examples/zustand/index.tsx
@@ -0,0 +1,23 @@
+import create from "zustand";
+interface Counter {
+  count: number;
+  inc: () => void;
+  dec: () => void;
+}
+
+const useStore = create<Counter>((set) => ({
+  count: 1,
+  inc: () => set((state) => ({ count: state.count + 1 })),
+  dec: () => set((state) => ({ count: state.count - 1 })),
+}));
+
+export const ZustandExample = () => {
+  const { count, inc, dec } = useStore();
+  return (
+    <div >
+      <button onClick={inc}>up</button>
+      <span>{count}</span>
+      <button onClick={dec}>down</button>
+    </div>
+  );
+};

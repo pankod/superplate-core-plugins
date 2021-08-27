a290ef6e0729b56dd375533fb2f08f784319066b
Salih <salihozdemir94@gmail.com>
2021-08-27 16:23:16 +0300

    add login page

---
+++ refine/plugins/google-auth-provider/src/pages/login.tsx
@@ -0,0 +1,44 @@
+import { AntdLayout, Button, Icons, useLogin } from "@pankod/refine";
+import { useGoogleLogin, GoogleLoginResponse } from "react-google-login";
+
+const { GoogleOutlined } = Icons;
+
+const clientId = "your-client-id";
+
+export const Login: React.FC = () => {
+    const { mutate: login, isLoading } = useLogin<GoogleLoginResponse>();
+
+    const { signIn } = useGoogleLogin({
+        onSuccess: (response) => login(response as GoogleLoginResponse),
+        clientId,
+        isSignedIn: true,
+        cookiePolicy: "single_host_origin",
+    });
+
+    return (
+        <AntdLayout
+            style={{
+                background: `radial-gradient(50% 50% at 50% 50%, #63386A 0%, #310438 100%)`,
+                backgroundSize: "cover",
+            }}
+        >
+            <div style={{ height: "100vh", display: "flex" }}>
+                <div style={{ maxWidth: "200px", margin: "auto" }}>
+                    <div style={{ marginBottom: "28px" }}>
+                        <img src="./refine.svg" alt="Refine" />
+                    </div>
+                    <Button
+                        type="primary"
+                        size="large"
+                        block
+                        icon={<GoogleOutlined />}
+                        loading={isLoading}
+                        onClick={() => signIn()}
+                    >
+                        Sign in
+                    </Button>
+                </div>
+            </div>
+        </AntdLayout>
+    );
+};

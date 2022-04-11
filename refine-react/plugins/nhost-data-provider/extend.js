a9e89236de4bec9b9132a08d35fdcb7cfca33315
Melih Ekinci <mmlhekinci@gmail.com>
2022-04-11 17:36:56 +0300

    nHost support (#264)
    * nHost support to refine-react
    
    * wrapper logic fixed and missing loginPage import added

---
+++ refine-react/plugins/nhost-data-provider/extend.js
@@ -0,0 +1,27 @@
+const base = {
+    _app: {
+        import: [
+            `import dataProvider from "@pankod/refine-nhost";`,
+            `import { authProvider } from "./authProvider";`,
+            `import { NhostAuthProvider } from "@nhost/react-auth";`,
+            "",
+            `import { nhost } from "utility";`,
+            
+        ],
+        refineProps: [
+            "dataProvider={dataProvider(nhost)}" , 
+            "authProvider={authProvider}" 
+        ],
+        refineAntdImports: [],
+        wrapper: [["<NhostAuthProvider nhost={nhost}>", "</NhostAuthProvider>"]],
+    },
+};
+module.exports = {
+    extend(answers) {
+        if (answers["uiFramework"] === "antd") {
+            base._app.refineAntdImports.push("LoginPage");
+            base._app.refineProps.push("LoginPage={LoginPage}");
+        }
+        return base;
+    },
+};
---
+++ refine-react/plugins/nhost-data-provider/src/utility/nhost.ts
@@ -0,0 +1,5 @@
+import { NhostClient } from "@nhost/nhost-js";
+
+export const nhost = new NhostClient({
+    backendUrl: "your_nhost_backend_url",
+});

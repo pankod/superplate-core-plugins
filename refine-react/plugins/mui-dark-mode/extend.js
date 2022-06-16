eaec9cb99452973f398436315e0f21ad520a9920
Salih <salihozdemir94@gmail.com>
2022-06-16 15:10:47 +0300

    feat: add header to dark mode plugin

--- refine-react/plugins/mui-dark-mode/extend.js
+++ refine-react/plugins/mui-dark-mode/extend.js
@@ -1,12 +1,16 @@
 const base = {
     _app: {
-        localImport: ['import { ColorModeContextProvider } from "./contexts";'],
+        localImport: [
+            'import { ColorModeContextProvider } from "./contexts";',
+            'import { Header } from "./components/header";'
+        ],
         wrapper: [
             ["<ColorModeContextProvider>", "</ColorModeContextProvider>"],
             ["<CssBaseline />"],
             [`<GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />`],
             ["<RefineSnackbarProvider>", "</RefineSnackbarProvider>"],
         ],
+        refineProps: ["Header={Header}"],
     },
 };
 

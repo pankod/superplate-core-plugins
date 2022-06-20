4fe8278d4a244a7290edf16258201e11a40e1b91
Salih <salihozdemir94@gmail.com>
2022-06-20 12:03:04 +0300

    feat: add mui-dark-mode plugin

---
+++ refine-nextjs/plugins/mui-dark-mode/extend.js
@@ -0,0 +1,30 @@
+const base = {
+    _app: {
+        localImport: [
+            'import { ColorModeContextProvider } from "contexts";',
+        ],
+        wrapper: [
+            ["<ColorModeContextProvider>", "</ColorModeContextProvider>"],
+            ["<CssBaseline />"],
+            [`<GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />`],
+            ["<RefineSnackbarProvider>", "</RefineSnackbarProvider>"],
+        ],
+        refineProps: [],
+    },
+};
+
+module.exports = {
+    extend() {
+        module.exports = {
+            extend(answers) {
+                if(answers["mui-custom-layout"] !== "mui-custom-layout") {
+                    base._app.localImport.push(`import { Header } from "components/layout"`)
+                    base._app.refineProps.push("Header={Header}")
+                }
+                return base;
+            },
+        };
+        
+        return base;
+    },
+};
---
+++ refine-nextjs/plugins/mui-dark-mode/src/components/layout/header/index.tsx
@@ -0,0 +1,45 @@
+import { useContext } from "react";
+import { useGetIdentity } from "@pankod/refine-core";
+import {
+  AppBar,
+  IconButton,
+  Avatar,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@pankod/refine-mui";
+import { DarkModeOutlined, LightModeOutlined } from "@mui/icons-material";
+
+import { ColorModeContext } from "contexts";
+
+export const Header: React.FC = () => {
+  const { mode, setMode } = useContext(ColorModeContext);
+
+  const { data: user } = useGetIdentity();
+  const showUserInfo = user && (user.name || user.avatar);
+
+  return (
+    <AppBar color="default" position="sticky" elevation={1}>
+      <Toolbar
+        variant="dense"
+        sx={{ display: "flex", justifyContent: "flex-end" }}
+      >
+        <IconButton
+          onClick={() => {
+            setMode();
+          }}
+        >
+          {mode === "dark" ? <LightModeOutlined /> : <DarkModeOutlined />}
+        </IconButton>
+        {showUserInfo && (
+          <Stack direction="row" gap="8px" alignItems="center">
+            {user.avatar && <Avatar src={user?.avatar} alt={user?.name} />}
+            {user.name && (
+              <Typography variant="subtitle2">{user?.name}</Typography>
+            )}
+          </Stack>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};

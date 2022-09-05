4487b9ff12610fa09f0694508d0f7b23a7449939
ozkalai <melihozkalayy@gmail.com>
2022-09-05 16:17:30 +0300

    fix: login pages

--- refine-react/plugins/auth-provider-google/src/pages/login.tsx
+++ refine-react/plugins/auth-provider-google/src/pages/login.tsx
@@ -1,41 +1,55 @@
-import { 
-    useLogin,
-    <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
-    useTranslate,
-    <%_ } _%>
-} from "@pankod/refine-core";
+import { useEffect, useRef } from "react";
+import { useLogin } from "@pankod/refine-core";
 <%_ if (answers["ui-framework"] === "antd") { _%>
     import { 
         AntdLayout, 
-        Button,
-        Icons,
     } from "@pankod/refine-antd";
-<%_ } _%> 
+<%_ } _%>
+<%_ if (answers["ui-framework"] === "mui") { _%>
+    import { 
+        Container, Box 
+    } from "@pankod/refine-mui";
+<%_ } _%>
 
-import { useGoogleLogin, GoogleLoginResponse } from "react-google-login";
-
-<%_ if (answers["ui-framework"] === "antd") { _%>
-const { GoogleOutlined } = Icons;
-<%_ } _%> 
+import { CredentialResponse } from "../interfaces/google";
 
 export const Login: React.FC = () => {
-    const { 
-        mutate: login,
-        <%_ if (answers["ui-framework"] === "antd") { _%> 
-        isLoading 
-        <%_ } _%> 
-    } = useLogin<GoogleLoginResponse>();
+    const { mutate: login } = useLogin<CredentialResponse>();
 
-    <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
-    const t = useTranslate();
-    <%_ } _%>
+    const GoogleButton = (): JSX.Element => {
+        const divRef = useRef<HTMLDivElement>(null);
 
-    const { signIn } = useGoogleLogin({
-        onSuccess: (response) => login(response as GoogleLoginResponse),
-        clientId: "your-client-id",
-        isSignedIn: true,
-        cookiePolicy: "single_host_origin",
-    });
+        useEffect(() => {
+            if (
+                typeof window === "undefined" ||
+                !window.google ||
+                !divRef.current
+            ) {
+                return;
+            }
+
+            try {
+                window.google.accounts.id.initialize({
+                    ux_mode: "popup",
+                    client_id: "your-client-id",
+                    callback: async (res: CredentialResponse) => {
+                        if (res.credential) {
+                            login(res);
+                        }
+                    },
+                });
+                window.google.accounts.id.renderButton(divRef.current, {
+                    theme: "filled_blue",
+                    size: "medium",
+                    type: "standard",
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        }, []); // you can also add your client id as dependency here
+
+        return <div ref={divRef} />;
+    };
 
     <%_ if (answers["ui-framework"] === "antd") { _%>
     return (
@@ -46,46 +60,64 @@ export const Login: React.FC = () => {
             }}
         >
             <div style={{ height: "100vh", display: "flex" }}>
-                <div style={{ maxWidth: "200px", margin: "auto" }}>
+                <div style={{ margin: "auto" }}>
                     <div style={{ marginBottom: "28px" }}>
                         <img src="./refine.svg" alt="Refine" />
                     </div>
-                    <Button
-                        type="primary"
-                        size="large"
-                        block
-                        icon={<GoogleOutlined />}
-                        loading={isLoading}
-                        onClick={() => signIn()}
-                    >
-                        <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
-                        {t("pages.login.signin", "Sign in")}
-                        <%_ } else { _%>
-                        Sign in
-                        <%_ } _%>
-                    </Button>
+                    <GoogleButton />
                 </div>
             </div>
-        </AntdLayout>    
+        </AntdLayout>
     );
-    <%_ } else {_%>
-        return(
-            <div
-                style={{
-                    height: "100vh",
+    <% } else if (answers["ui-framework"] === "mui") { _%>
+    return (
+        <Box
+        component="div"
+        sx={{
+            background: `radial-gradient(50% 50% at 50% 50%, #63386A 0%, #310438 100%)`,
+            backgroundSize: "cover",
+        }}
+    >
+        <Container
+            component="main"
+            maxWidth="xs"
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                height: "100vh",
+            }}
+        >
+            <Box
+                sx={{
                     display: "flex",
                     justifyContent: "center",
+                    flexDirection: "column",
                     alignItems: "center",
                 }}
             >
-                <button onClick={() => signIn()}>
-                    <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
-                    {t("pages.login.signin", "Sign in")}
-                    <%_ } else { _%>
-                        Sign in with Google
-                    <%_ } _%>
-                </button>
-            </div>
-        );
+                <div>
+                    <img src="./refine.svg" alt="Refine Logo" />
+                </div>
+                <Box mt={4}>
+                    <GoogleButton />
+                </Box>
+            </Box>
+        </Container>
+    </Box>
+    );
+    <%_ } else {_%>
+    return (
+        <div
+            style={{
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+        <GoogleButton />
+        </div>
+    );
     <%_ } _%>   
 };

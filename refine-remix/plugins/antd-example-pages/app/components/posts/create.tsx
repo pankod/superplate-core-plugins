a7ab84ae0119d261cc90575b244f447433140b49
Ali Emir Sen <senaliemir@gmail.com>
2022-09-22 17:05:49 +0300

    fix: remove i18n in remix-antd

--- refine-remix/plugins/antd-example-pages/app/components/posts/create.tsx
+++ refine-remix/plugins/antd-example-pages/app/components/posts/create.tsx
@@ -1,10 +1,5 @@
 import { useState } from "react";
-import {
-    IResourceComponentsProps,
-    <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
-        useTranslate,
-    <%_ } _%>
-} from "@pankod/refine-core";
+import { IResourceComponentsProps } from "@pankod/refine-core";
 import {
     Create,
     Form,
@@ -24,10 +19,6 @@ export const PostCreate: React.FC<IResourceComponentsProps> = () => {
         "write",
     );
 
-    <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
-    const t = useTranslate();
-    <%_ } _%>
-
     const { formProps, saveButtonProps } = useForm<IPost>();
 
     const { selectProps: categorySelectProps } = useSelect<ICategory>({
@@ -38,12 +29,8 @@ export const PostCreate: React.FC<IResourceComponentsProps> = () => {
         <Create saveButtonProps={saveButtonProps}>
             <Form {...formProps} layout="vertical">
                 <Form.Item
-                    <%_ if (answers[`i18n-${answers["ui-framework"]}`] === "no") { _%>
                     label="Title"
-                    <%_ } else { _%>
-                    label={t("posts.fields.title")}
-                    <%_ } _%>
-                    name="title"    
+                    name="title"
                     rules={[
                         {
                             required: true,
@@ -53,11 +40,7 @@ export const PostCreate: React.FC<IResourceComponentsProps> = () => {
                     <Input />
                 </Form.Item>
                 <Form.Item
-                    <%_ if (answers[`i18n-${answers["ui-framework"]}`] === "no") { _%>
                     label="Status"
-                    <%_ } else { _%>
-                    label={t("posts.fields.status.title")}
-                    <%_ } _%>
                     name="status"
                     rules={[
                         {
@@ -65,62 +48,37 @@ export const PostCreate: React.FC<IResourceComponentsProps> = () => {
                         },
                     ]}
                 >
-                    <Select 
-                     <%_ if (answers[`i18n-${answers["ui-framework"]}`] === "no") { _%>
-                     options={[
-                        {
-                          label: "published",
-                          value: "published",
-                        },
-                        {
-                          label:  "draft",
-                          value: "draft",
-                        },
-                        {
-                          label: "rejected",
-                          value: "rejected",
-                        },
-                      ]}
-                      <%_ } else { _%>
+                    <Select
                         options={[
                             {
-                              label: t("posts.fields.status.published"),
-                              value: "published",
+                                label: "published",
+                                value: "published",
                             },
                             {
-                              label:  t("posts.fields.status.draft"),
-                              value: "draft",
+                                label: "draft",
+                                value: "draft",
                             },
                             {
-                              label: t("posts.fields.status.rejected"),
-                              value: "rejected",
+                                label: "rejected",
+                                value: "rejected",
                             },
-                          ]}
-                    <%_ } _%>
+                        ]}
                     />
                 </Form.Item>
 
-
                 <Form.Item
-                <%_ if (answers[`i18n-${answers["ui-framework"]}`] === "no") { _%>
                     label="Category"
-                <%_ } else { _%>
-                    label={t("posts.fields.category.title")}
-                <%_ } _%>
-                name={["category", "id"]}
-                rules={[
-                    {
-                    required: true,
-                    },
-                ]}>
-                <Select {...categorySelectProps} />
+                    name={["category", "id"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Select {...categorySelectProps} />
                 </Form.Item>
                 <Form.Item
-                    <%_ if (answers[`i18n-${answers["ui-framework"]}`] === "no") { _%>
                     label="Content"
-                    <%_ } else { _%>
-                    label={t("posts.fields.content")}
-                    <%_ } _%>
                     name="content"
                     rules={[
                         {

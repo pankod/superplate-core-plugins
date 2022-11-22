95702035bb84e1b4f5dc162f87c7041152136fdf
Ömer Faruk APLAK <omer@pankod.com>
2022-11-22 12:43:40 +0300

    fix: tailwindcss nextjs

--- nextjs/plugins/tailwind/tailwind.config.js
+++ nextjs/plugins/tailwind/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     content: [
         "./pages/**/*.{html,js,ts,tsx}",
-        "./components/**/*.{html,js,ts,tsx}",
+        "./src/components/**/*.{html,js,ts,tsx}",
     ],
     theme: {
         extend: {},

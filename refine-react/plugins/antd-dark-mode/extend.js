0a9601e82bfaf671c79917abab1289072e236fb6
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-12-21 12:32:57 +0300

    feat: add antd dark mode support

---
+++ refine-react/plugins/antd-dark-mode/extend.js
@@ -0,0 +1,29 @@
+const base = {
+    _app: {
+        refineAntdImports: [],
+        wrapper: [
+            [`<ColorModeContextProvider>`, `</ColorModeContextProvider>`]
+        ],
+        innerHooks: [],
+        inner: [],
+        localImport: [
+            `import { ColorModeContextProvider } from "contexts";`,
+        ],
+    },
+};
+
+module.exports = {
+    extend() {
+        module.exports = {
+            extend(answers) {
+                if (answers["antd-custom-layout"] !== "antd-custom-layout") {
+                    base._app.localImport.push(`import { Header } from "components/layout"`)
+                    base._app.refineProps.push("Header={Header}")
+                }
+                return base;
+            },
+        };
+
+        return base;
+    },
+};

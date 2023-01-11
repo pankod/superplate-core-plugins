57ef109d2602c57f1adece409f35757ff0da7a80
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-01-11 16:09:52 +0300

    fix: mui custom-layout and dark-mode support

--- refine-nextjs/plugins/mui-dark-mode/src/components/layout/index.ts
+++ refine-nextjs/plugins/mui-dark-mode/src/components/layout/index.ts
@@ -1,1 +1,6 @@
 export * from "./header";
+<%_ if (answers["mui-custom-layout"] === "mui-custom-layout") { _%>
+export * from "./sider";
+export * from "./layout";
+export * from "./title";
+<%_ } _%>

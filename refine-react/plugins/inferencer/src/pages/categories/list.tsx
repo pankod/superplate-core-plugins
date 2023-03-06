d4af29dc3dede7087999d950fc32dc6219f0b67a
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-06 11:15:25 +0300

    feat: upgrate inferencer

---
+++ refine-react/plugins/inferencer/src/pages/categories/list.tsx
@@ -0,0 +1,6 @@
+import { IResourceComponentsProps } from "@pankod/refine-core";
+import { <%- (_app.inferencer.componentPrefix || "") _%>ListInferencer } from "@pankod/refine-inferencer/<%- (_app.inferencer.folder || "") _%>";
+
+export const CategoryList: React.FC<IResourceComponentsProps> = () => {
+    return <<%- (_app.inferencer.componentPrefix || "") _%>ListInferencer />;
+};

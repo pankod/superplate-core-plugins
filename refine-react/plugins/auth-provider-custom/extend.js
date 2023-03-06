508d9828933a39d16700b30121c40e2c1cdb695b
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-06 11:16:02 +0300

    feat: upgrate custom auth provider

--- refine-react/plugins/auth-provider-custom/extend.js
+++ refine-react/plugins/auth-provider-custom/extend.js
@@ -2,10 +2,19 @@ const base = {
     _app: {
         relativeImport: ['import { authProvider } from "./authProvider";'],
         refineProps: ["authProvider={authProvider}"],
+        refineImports: [`Authenticated`],
         refineAntdImports: [],
         refineMantineImports: [],
         refineMuiImports: [],
         refineChakraImports: [],
+        layoutWrapper: [
+            [`<Authenticated fallback={
+                <Routes>
+                    <Route path="/login" element={<AuthPage type="login" />} />
+                    <Route path="*" element={<Navigate to="/login" />} />
+                </Routes>
+            }>`, `</Authenticated>`],
+        ],
     },
 };
 
@@ -13,22 +22,18 @@ module.exports = {
     extend(answers) {
         if (answers["ui-framework"] === "antd") {
             base._app.refineAntdImports.push("AuthPage");
-            base._app.refineProps.push("LoginPage={AuthPage}");
         }
 
         if (answers["ui-framework"] === "mui") {
             base._app.refineMuiImports.push("AuthPage");
-            base._app.refineProps.push("LoginPage={AuthPage}");
         }
 
         if (answers["ui-framework"] === "mantine") {
             base._app.refineMantineImports.push("AuthPage");
-            base._app.refineProps.push("LoginPage={AuthPage}");
         }
 
         if (answers["ui-framework"] === "chakra") {
             base._app.refineChakraImports.push("AuthPage");
-            base._app.refineProps.push("LoginPage={AuthPage}");
         }
         return base;
     },
--- refine-react/plugins/auth-provider-custom/src/authProvider.ts
+++ refine-react/plugins/auth-provider-custom/src/authProvider.ts
@@ -1,37 +1,57 @@
-import { AuthProvider } from "@pankod/refine-core";
+import { AuthBindings } from "@pankod/refine-core";
 
 export const TOKEN_KEY = "refine-auth";
 
-export const authProvider: AuthProvider = {
+export const authProvider: AuthBindings = {
     login: async ({ username, email, password }) => {
         if ((username || email) && password) {
             localStorage.setItem(TOKEN_KEY, username);
-            return Promise.resolve();
-        }
-        return Promise.reject(new Error("username: admin, password: admin"));
-    },
-    logout: () => {
-        localStorage.removeItem(TOKEN_KEY);
-        return Promise.resolve();
-    },
-    checkError: () => Promise.resolve(),
-    checkAuth: () => {
-        const token = localStorage.getItem(TOKEN_KEY);
-        if (token) {
-            return Promise.resolve();
-        }
-
-        return Promise.reject();
-    },
-    getPermissions: () => Promise.resolve(),
-    getUserIdentity: async () => {
-        const token = localStorage.getItem(TOKEN_KEY);
-        if (!token) {
-            return Promise.reject();
+            return Promise.resolve({
+                success: true,
+                redirectTo: "/",
+            });
         }
 
         return Promise.resolve({
-            id: 1,
+            success: false,
+            error: {
+                name: "LoginError",
+                message: "Invalid username or password",
+            },
+        });
+    },
+    logout: () => {
+        localStorage.removeItem(TOKEN_KEY);
+        return Promise.resolve({
+            success: true,
+        });
+    },
+    check: () => {
+        const token = localStorage.getItem(TOKEN_KEY);
+        const authenticated = !!token;
+        return Promise.resolve({
+            authenticated,
+        });
+    },
+    getPermissions: () => Promise.resolve(),
+    getIdentity: async () => {
+        const token = localStorage.getItem(TOKEN_KEY);
+        if (token) {
+            return Promise.resolve({
+                id: 1,
+                name: "John Doe",
+                avatar: "https://i.pravatar.cc/300",
+            });
+        }
+    },
+    onError: (err) => {
+        console.log(err);
+        return Promise.resolve({
+            error: {
+                name: "LoginError",
+                message: err,
+            },
+            logout: true,
         });
     },
 };

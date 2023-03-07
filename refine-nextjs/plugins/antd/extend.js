c2ae237a622811cb8c7d369d0b4f6565d8325a98
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-07 14:58:32 +0300

    feat: update for antd

--- refine-nextjs/plugins/antd/extend.js
+++ refine-nextjs/plugins/antd/extend.js
@@ -2,22 +2,19 @@ const base = {
     _app: {
         refineProps: ["notificationProvider={notificationProvider}"],
         import: ['import "@pankod/refine-antd/dist/reset.css";'],
-        refineAntdImports: ["notificationProvider"],
+        refineAntdImports: ["notificationProvider", "Layout"],
+        wrapper: [
+            [`<ColorModeContextProvider>`, `</ColorModeContextProvider>`]
+        ],
+        localImport: [
+            `import { Header } from "@components/header"`,
+            `import { ColorModeContextProvider } from "@contexts";`,
+        ],
     },
 };
 
 module.exports = {
     extend(answers) {
-        if (answers["antd-custom-layout"] === "no") {
-            base._app.refineAntdImports.push("Layout");
-            base._app.refineProps.push("Layout={Layout}");
-        }
-
-        base._app.refineAntdImports.push("ReadyPage");
-        base._app.refineAntdImports.push("ErrorComponent");
-        base._app.refineProps.push("ReadyPage={ReadyPage}");
-        base._app.refineProps.push("catchAll={<ErrorComponent />}");
-
         return base;
     },
 };
--- refine-nextjs/plugins/i18n-antd/src/components/header/index.tsx
+++ refine-nextjs/plugins/i18n-antd/src/components/header/index.tsx
@@ -1,13 +1,11 @@
-<%_ if (answers["antd-dark-mode"] === "antd-dark-mode") { _%>
-
-  import { useContext } from "react";
+import { useContext } from "react";
 import {
   useGetLocale,
   useSetLocale,
   useGetIdentity,
 } from "@pankod/refine-core";
 import {
-  AntdLayout,
+  Layout as AntdLayout,
   Space,
   Menu,
   Button,
@@ -16,7 +14,7 @@ import {
   Avatar,
   Typography,
   Switch,
-} from "@pankod/refine-antd";
+} from "antd";
 import { useRouter } from "next/router";
 import NextRouter from "@pankod/refine-nextjs-router";
 
@@ -88,86 +86,4 @@ export const Header: React.FC = () => {
     </AntdLayout.Header>
   );
 };
-
-
-<%_ } else { _%>
-
-import { useContext } from "react";
-import { useGetLocale, useGetIdentity } from "@pankod/refine-core";
-import {
-  AntdLayout,
-  Space,
-  Menu,
-  Button,
-  Icons,
-  Dropdown,
-  Avatar,
-  Typography,
-  Switch,
-} from "@pankod/refine-antd";
-import { useRouter } from "next/router";
-import NextRouter from "@pankod/refine-nextjs-router";
-
-const { DownOutlined } = Icons;
-const { Text } = Typography;
-const { Link } = NextRouter;
-
-export const Header: React.FC = () => {
-  const locale = useGetLocale();
-  const { locales } = useRouter();
-  const currentLocale = locale();
-  const { data: user } = useGetIdentity();
-
-  const menu = (
-    <Menu selectedKeys={currentLocale ? [currentLocale] : []}>
-      {[...(locales || [])].sort().map((lang: string) => (
-        <Menu.Item
-          key={lang}
-          icon={
-            <span style={{ marginRight: 8 }}>
-              <Avatar size={16} src={`/images/flags/${lang}.svg`} />
-            </span>
-          }
-        >
-          <Link href="/" locale={lang}>
-            {lang === "en" ? "English" : "German"}
-          </Link>
-        </Menu.Item>
-      ))}
-    </Menu>
-  );
-
-  return (
-    <AntdLayout.Header
-      style={{
-        display: "flex",
-        justifyContent: "flex-end",
-        alignItems: "center",
-        padding: "0px 24px",
-        height: "64px",
-      }}
-    >
-      <Dropdown overlay={menu}>
-        <Button type="link">
-          <Space>
-            <Avatar size={16} src={`/images/flags/${currentLocale}.svg`} />
-            {currentLocale === "en" ? "English" : "German"}
-            <DownOutlined />
-          </Space>
-        </Button>
-      </Dropdown>
-      <Space style={{ marginLeft: "8px" }}>
-        {user?.name && (
-          <Text ellipsis strong>
-            {user.name}
-          </Text>
-        )}
-        {user?.avatar && <Avatar src={user?.avatar} alt={user?.name} />}
-      </Space>
-    </AntdLayout.Header>
-  );
-};
-
-<%_ } _%>
-
 

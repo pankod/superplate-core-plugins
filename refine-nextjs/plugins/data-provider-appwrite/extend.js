680ea36d306b638266dd259291e8aedcbe7db522
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-09 17:19:25 +0300

    fix: appwrite data provider

--- refine-nextjs/plugins/data-provider-appwrite/extend.js
+++ refine-nextjs/plugins/data-provider-appwrite/extend.js
@@ -14,7 +14,6 @@ const base = {
             `liveProvider={liveProvider(appwriteClient, {
                 databaseId: "default",
             })}`,
-            `liveMode="auto"`,
             `authProvider={authProvider}`,
         ],
         refineAntdImports: [],
@@ -23,21 +22,7 @@ const base = {
     },
 };
 module.exports = {
-    extend(answers) {
-        if (answers["ui-framework"] === "antd") {
-            base._app.refineAntdImports.push("AuthPage");
-            base._app.refineProps.push("LoginPage={AuthPage}");
-        }
-
-        if (answers["ui-framework"] === "mui") {
-            base._app.refineMuiImports.push("AuthPage");
-            base._app.refineProps.push("LoginPage={AuthPage}");
-        }
-
-        if (answers["ui-framework"] === "mantine") {
-            base._app.refineMantineImports.push("AuthPage");
-            base._app.refineProps.push("LoginPage={AuthPage}");
-        }
+    extend() {
         return base;
     },
 };

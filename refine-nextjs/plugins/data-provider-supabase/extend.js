d68517efb7e2b2e0022436264fa6d231335daab6
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-11 17:12:45 +0300

    fix: remove AuthPage on refineImports

--- refine-nextjs/plugins/data-provider-supabase/extend.js
+++ refine-nextjs/plugins/data-provider-supabase/extend.js
@@ -19,22 +19,18 @@ module.exports = {
     extend(answers) {
         if (answers["ui-framework"] === "antd") {
             base._app.refineAntdImports.push("AuthPage");
-            base._app.refineProps.push("LoginPage={AuthPage}");
         }
 
         if (answers["ui-framework"] === "mui") {
             base._app.refineMuiImports.push("AuthPage");
-            base._app.refineProps.push("LoginPage={AuthPage}");
         }
 
         if (answers["ui-framework"] === "mantine") {
             base._app.refineMantineImports.push("AuthPage");
-            base._app.refineProps.push("LoginPage={AuthPage}");
         }
 
         if (answers["ui-framework"] === "chakra") {
             base._app.refineChakraImports.push("AuthPage");
-            base._app.refineProps.push("LoginPage={AuthPage}");
         }
         return base;
     },

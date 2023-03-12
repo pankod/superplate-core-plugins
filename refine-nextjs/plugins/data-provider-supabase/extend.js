36a41ec0cd325ab2fbccad2e49873651638588fa
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-12 13:48:55 +0300

    fix: auth page imports

--- refine-nextjs/plugins/data-provider-supabase/extend.js
+++ refine-nextjs/plugins/data-provider-supabase/extend.js
@@ -16,22 +16,7 @@ const base = {
     },
 };
 module.exports = {
-    extend(answers) {
-        if (answers["ui-framework"] === "antd") {
-            base._app.refineAntdImports.push("AuthPage");
-        }
-
-        if (answers["ui-framework"] === "mui") {
-            base._app.refineMuiImports.push("AuthPage");
-        }
-
-        if (answers["ui-framework"] === "mantine") {
-            base._app.refineMantineImports.push("AuthPage");
-        }
-
-        if (answers["ui-framework"] === "chakra") {
-            base._app.refineChakraImports.push("AuthPage");
-        }
+    extend() {
         return base;
     },
 };

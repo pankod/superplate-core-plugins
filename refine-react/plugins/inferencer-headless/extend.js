d4ab034845a2bbf99ff66914ba4e721317e74f02
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-14 15:51:16 +0300

    fix: headless routes

--- refine-react/plugins/inferencer-headless/extend.js
+++ refine-react/plugins/inferencer-headless/extend.js
@@ -1,3 +1,94 @@
+function getAuth(answers) {
+
+    if (answers["auth-provider"] === 'auth-provider-auth0') {
+        return `<Route path="/login" element={<Login />} />`
+    }
+    return `<Route path="/login" element={<AuthPage type="login" formProps={{` + formProps(answers) + `}}/>} />`
+
+}
+
+function getRoutes(withAuth, answers) {
+    if (withAuth === true) {
+        return `<Route
+        element={
+            <Authenticated
+                fallback={<CatchAllNavigate to="/login" />}
+            >
+                <Outlet />
+            </Authenticated>
+        }
+    >
+        <Route index element={<NavigateToResource resource="products" />} />
+        <Route path="/products">
+            <Route index element={<ProductList />} />
+            <Route path="create" element={<ProductCreate />} />
+            <Route path="edit/:id" element={<ProductEdit />} />
+            <Route path="show/:id" element={<ProductShow />} />
+        </Route>
+        <Route path="/categories">
+            <Route index element={<CategoryList />} />
+            <Route path="create" element={<CategoryCreate />} />
+            <Route path="edit/:id" element={<CategoryEdit />} />
+            <Route path="show/:id" element={<CategoryShow />} />
+        </Route>
+    </Route>
+    <Route
+        element={
+            <Authenticated fallback={<Outlet />}>
+                <NavigateToResource />
+            </Authenticated>
+        }
+    >
+    `+ getAuth(answers) + `
+    </Route>
+    <Route
+        element={
+            <Authenticated>
+                <Outlet />
+            </Authenticated>
+        }
+    >
+        <Route path="*" element={<ErrorComponent />} />
+    </Route>`;
+
+    }
+
+    return `
+    <Route index element={<NavigateToResource resource="products" />} />
+    <Route path="/products">
+        <Route index element={<ProductList />} />
+        <Route path="create" element={<ProductCreate />} />
+        <Route path="edit/:id" element={<ProductEdit />} />
+        <Route path="show/:id" element={<ProductShow />} />
+    </Route>
+    <Route path="/categories">
+        <Route index element={<CategoryList />} />
+        <Route path="create" element={<CategoryCreate />} />
+        <Route path="edit/:id" element={<CategoryEdit />} />
+        <Route path="show/:id" element={<CategoryShow />} />
+    </Route>
+    <Route path="*" element={<ErrorComponent />} />
+    `;
+}
+
+
+function formProps(answers) {
+    if (answers["ui-framework"] === 'antd' || answers["ui-framework"] === 'mantine') {
+        if (answers["data-provider"] === 'data-provider-supabase') {
+            return `initialValues:{ email: "info@refine.dev", password: "refine-supabase" }`
+        } else {
+            return `initialValues:{ email: "demo@refine.dev", password: "demodemo" }`
+        }
+    } else if (answers["ui-framework"] === 'mui' || answers["ui-framework"] === 'chakra') {
+        if (answers["data-provider"] === 'data-provider-supabase') {
+            return `defaultValues:{ email: "info@refine.dev", password: "refine-supabase" }`
+        } else {
+            return `defaultValues:{ email: "demo@refine.dev", password: "demodemo" }`
+        }
+    }
+}
+
+
 const base = {
     _app: {
         import: [],
@@ -21,13 +112,16 @@ const base = {
                 },
             ]}`
         ],
+        refineAntdImports: [],
+        refineImports: [`AuthPage`, `Authenticated`],
+        wrapper: [],
+        inferencer: {},
         localImport: [
             `import { ProductList, ProductCreate, ProductEdit, ProductShow } from "pages/products";`,
             `import { CategoryList, CategoryCreate, CategoryEdit, CategoryShow } from "pages/categories";`
         ],
-        refineAntdImports: [],
-        wrapper: [
-        ],
+        routes: [],
+        inferencer: {}
     },
 };
 
@@ -39,28 +133,20 @@ module.exports = {
             componentPrefix: "Headless",
         };
 
-        // if auth-provider is none
-        if (answers["auth-provider"] === "none") {
-            base._app.routes = [
-                `<Route element={<Outlet />}>
-                    <Route index element={<NavigateToResource resource="products" />} />
-                    <Route path="/products">
-                        <Route index element={<ProductList />} />
-                        <Route path="/products/create" element={<ProductCreate />} />
-                        <Route path="/products/edit/:id" element={<ProductEdit />} />
-                        <Route path="/products/show/:id" element={<ProductShow />} />
-                    </Route>
-                    <Route path="/categories">
-                        <Route index element={<CategoryList />} />
-                        <Route path="/products/create" element={<CategoryCreate />} />
-                        <Route path="/products/edit/:id" element={<CategoryEdit />} />
-                        <Route path="/products/show/:id" element={<CategoryShow />} />
-                    </Route>
-                </Route>`,
-                `<Route element={<Outlet />}>
-                    <Route path="*" element={<div>Error</div>} />
-                </Route>`
-            ];
+        if (
+            answers["data-provider"] === "data-provider-appwrite" ||
+            answers["data-provider"] === "data-provider-supabase" ||
+            answers["auth-provider"] === "auth-provider-auth0" ||
+            answers["data-provider"] === "data-provider-strapi-v4"
+        ) {
+            const routes = getRoutes(true, answers);
+            base._app.routes = [routes];
+        } else if (answers["auth-provider"] === "none") {
+            const routes = getRoutes(false, answers);
+            base._app.routes = [routes];
+        } else {
+            const routes = getRoutes(true, answers);
+            base._app.routes = [routes];
         }
 
         return base;

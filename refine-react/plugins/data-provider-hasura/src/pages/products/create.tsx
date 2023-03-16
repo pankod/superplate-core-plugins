79e444547d2981339ed14581525866ebe06124b5
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-16 11:58:40 +0300

    fix: remove hasura pages

--- refine-react/plugins/data-provider-hasura/src/pages/products/create.tsx
+++
@@ -1,6 +0,0 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-
-// TODO: Implement Product Create Page
-export const ProductCreate: React.FC<IResourceComponentsProps> = () => {
-    return <div>Product Create Page</div>;
-};
--- refine-react/plugins/data-provider-hasura/src/pages/products/list.tsx
+++
@@ -1,6 +0,0 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-
-export const ProductList: React.FC<IResourceComponentsProps> = () => {
-    // TODO: Implement Product List Page
-    return <div>Product List Page</div>;
-};

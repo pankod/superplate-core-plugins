0f2d8eeb97ffcb0d693ab6c0bc494a2d544053cc
Salih <salihozdemir94@gmail.com>
2023-03-24 16:46:29 +0300

    fix(remix): update the  import path

--- refine-remix/plugins/data-provider-appwrite/app/routes/_index.tsx
+++ refine-remix/plugins/data-provider-appwrite/app/routes/_index.tsx
@@ -1,15 +1,4 @@
-<%_ if (answers["ui-framework"] === 'antd') { _%>
-     import { WelcomePage } from "@refinedev/antd";
- <%_ } _%>
- <%_ if (answers["ui-framework"] === 'mui') { _%>
-     import { WelcomePage } from "@refinedev/mui";
- <%_ } _%>
- <%_ if (answers["ui-framework"] === 'mantine') { _%>
-     import { WelcomePage } from "@refinedev/mantine";
- <%_ } _%>
- <%_ if (answers["ui-framework"] === 'chakra') { _%>
-     import { WelcomePage } from "@refinedev/chakra-ui";
- <%_ } _%>
+import { WelcomePage } from "@refinedev/core";
 import type { LoaderArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 

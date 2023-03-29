72654ba7b724e3238b2d825a43a8a470f1bdaeda
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-29 16:17:45 +0300

    feat: redesing cra keycloak

--- refine-react/plugins/auth-provider-keycloak/src/pages/login.tsx
+++ refine-react/plugins/auth-provider-keycloak/src/pages/login.tsx
@@ -1,16 +1,21 @@
 import { useTranslate, useLogin } from "@refinedev/core";
 <%_ if (answers["ui-framework"] === "antd") { _%>
-import { Button } from "antd";
+import { ThemedTitle } from "@refinedev/antd";
+import { Button, Typography, Layout, Space } from "antd";
 <%_ } _%> 
 <%_ if (answers["ui-framework"] === 'mui') { _%>
-    import { Button } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
+import { ThemedTitle } from "@refinedev/mui";
 <%_ } _%>
 <%_ if (answers["ui-framework"] === 'mantine') { _%>
-    import { Button } from "@mantine/core";
+import { Button, Box, Space, Text } from "@mantine/core";
+import { ThemedTitle } from "@refinedev/mantine";
 <%_ } _%>
 <%_ if (answers["ui-framework"] === 'chakra') { _%>
-    import { Button } from "@chakra-ui/react";
+import { Button, Box, Text, VStack } from "@chakra-ui/react";
+import { ThemedTitle } from "@refinedev/chakra-ui";
 <%_ } _%>
+
 
 export const Login: React.FC = () => {
     const { mutate: login } = useLogin();
@@ -19,32 +24,170 @@ export const Login: React.FC = () => {
     const t = useTranslate();
     <%_ } _%>
 
-    return(
-        <div
+    <%_ if (answers["ui-framework"] === "antd") { _%>
+        return (
+            <Layout
+            style={{
+                height: "100vh",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+            <Space direction="vertical" align="center" size="large">
+                <ThemedTitle
+                collapsed={false}
+                wrapperStyles={{
+                    fontSize: "22px",
+                }}
+                />
+                <Button
+                    type="primary"
+                    size="middle"
+                    onClick={() => login({})}
+                >
+                    <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
+                    {t("pages.login.signin", "Sign in")}
+                    <%_ } else { _%>
+                        Sign in
+                    <%_ } _%>
+                </Button>
+                <Typography.Text type="secondary">
+                Powered by Keycloak
+                </Typography.Text>
+            </Space>
+            </Layout>
+        );
+    <%_ } _%> 
+    
+    <%_ if (answers["ui-framework"] === 'mui') { _%>
+        return (
+            <Container
             style={{
                 height: "100vh",
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
             }}
-        >
-            <%_ if (answers["ui-framework"] === "no") { _%>
-                <button onClick={() => login({})}>
+            >
+            <Box
+                display="flex"
+                gap="25px"
+                justifyContent="center"
+                flexDirection="column"
+            >
+                <ThemedTitle
+                collapsed={false}
+                wrapperStyles={{
+                    fontSize: "22px",
+                }}
+                />
+
+                <Button variant="contained" onClick={() => login({})}>
                     <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
                     {t("pages.login.signin", "Sign in")}
                     <%_ } else { _%>
                         Sign in
                     <%_ } _%>
-            </button>
-            <%_ } else { _%>
-                <Button onClick={() => login({})}>
+                </Button>
+                <Typography align="center" color={"text.secondary"} fontSize="14px">
+                Powered by Keycloak
+                </Typography>
+            </Box>
+            </Container>
+        );
+    <%_ } _%>
+
+    <%_ if (answers["ui-framework"] === 'mantine') { _%>
+        return (
+            <Box
+            sx={{
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+            <ThemedTitle
+                collapsed={false}
+                wrapperStyles={{
+                fontSize: "22px",
+                }}
+            />
+            <Space h="xl" />
+
+            <Button
+                type="button"
+                variant="filled"
+                onClick={() => login({})}
+            >
+                <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
+                    {t("pages.login.signin", "Sign in")}
+                <%_ } else { _%>
+                    Sign in
+                <%_ } _%>
+            </Button>
+            <Space h="lg" />
+            <Text fz="sm" color="gray">
+                Powered by Keycloak
+            </Text>
+            </Box>
+        );
+    <%_ } _%>
+
+    <%_ if (answers["ui-framework"] === 'chakra') { _%>
+        return (
+            <Box
+            sx={{
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+            <VStack spacing="8" align="stretch">
+                <ThemedTitle
+                collapsed={false}
+                wrapperStyles={{
+                    fontSize: "22px",
+                }}
+                />
+
+                <Button colorScheme="blue" onClick={() => login({})}>
+                    <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
+                    {t("pages.login.signin", "Sign in")}
+                    <%_ } else { _%>
+                        Sign in
+                    <%_ } _%>
+                </Button>
+
+                <Text alignItems="center" display="inherit" fontSize="xs" color="gray">
+                Powered by Keycloak
+                </Text>
+            </VStack>
+            </Box>
+        );
+    <%_ } _%>
+
+    <%_ if (answers["ui-framework"] === "no") { _%>
+        return(
+            <div
+                style={{
+                    height: "100vh",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+            <button onClick={() => login({})}>
                 <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
                 {t("pages.login.signin", "Sign in")}
                 <%_ } else { _%>
-                Sign in
+                    Sign in
                 <%_ } _%>
-            </Button>
-            <%_ } _%>                
-        </div>
-    );
+            </button>               
+            </div>
+        );
+    <%_ } _%>
 };

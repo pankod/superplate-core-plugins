a2245f4382c9eeca48a06feb0f3cb5edc54e3595
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-29 15:26:19 +0300

    feat: redesing cra google

--- refine-react/plugins/auth-provider-google/src/pages/login.tsx
+++ refine-react/plugins/auth-provider-google/src/pages/login.tsx
@@ -1,5 +1,23 @@
 import { useEffect, useRef } from "react";
 import { useLogin } from "@refinedev/core";
+
+<%_ if (answers["ui-framework"] === "antd") { _%>
+import { ThemedTitle } from "@refinedev/antd";
+import { Typography, Layout, Space } from "antd";
+<%_ } _%> 
+<%_ if (answers["ui-framework"] === 'mui') { _%>
+import { Box, Container, Typography } from "@mui/material";
+import { ThemedTitle } from "@refinedev/mui";
+<%_ } _%>
+<%_ if (answers["ui-framework"] === 'mantine') { _%>
+import { Box, Space, Text } from "@mantine/core";
+import { ThemedTitle } from "@refinedev/mantine";
+<%_ } _%>
+<%_ if (answers["ui-framework"] === 'chakra') { _%>
+import { Box, Text, VStack } from "@chakra-ui/react";
+import { ThemedTitle } from "@refinedev/chakra-ui";
+<%_ } _%>
+
 import { CredentialResponse } from "../interfaces/google";
 
 // Todo: Update your Google Client ID here
@@ -43,16 +61,152 @@ export const Login: React.FC = () => {
         return <div ref={divRef} />;
     };
 
-    return (
-        <div
+     const GoogleIcon = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="13"
+      height="13"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8"></path>
+    </svg>
+  );
+
+
+    <%_ if (answers["ui-framework"] === "antd") { _%>
+        return (
+            <Layout
+            style={{
+                height: "100vh",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+            <Space direction="vertical" align="center" size="large">
+                <ThemedTitle
+                collapsed={false}
+                wrapperStyles={{
+                    fontSize: "22px",
+                }}
+                />
+                <GoogleButton />
+                <Typography.Text type="secondary">
+                Powered by <GoogleIcon /> Auth0
+                </Typography.Text>
+            </Space>
+            </Layout>
+        );
+    <%_ } _%> 
+    
+    <%_ if (answers["ui-framework"] === 'mui') { _%>
+        return (
+            <Container
             style={{
                 height: "100vh",
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
             }}
-        >
+            >
+            <Box
+                display="flex"
+                gap="25px"
+                justifyContent="center"
+                flexDirection="column"
+            >
+                <ThemedTitle
+                collapsed={false}
+                wrapperStyles={{
+                    fontSize: "22px",
+                }}
+                />
+
+                <GoogleButton />
+
+                <Typography align="center" color={"text.secondary"} fontSize="14px">
+                Powered by <GoogleIcon /> Auth0
+                </Typography>
+            </Box>
+            </Container>
+        );
+    <%_ } _%>
+
+    <%_ if (answers["ui-framework"] === 'mantine') { _%>
+        return (
+            <Box
+            sx={{
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+            <ThemedTitle
+                collapsed={false}
+                wrapperStyles={{
+                fontSize: "22px",
+                }}
+            />
+            <Space h="xl" />
+
             <GoogleButton />
-        </div>
-    );
+
+            <Space h="lg" />
+            <Text fz="sm" color="gray">
+                Powered by <GoogleIcon /> Auth0
+            </Text>
+            </Box>
+        );
+    <%_ } _%>
+
+    <%_ if (answers["ui-framework"] === 'chakra') { _%>
+        return (
+            <Box
+            sx={{
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+            <VStack spacing="8" align="stretch">
+                <ThemedTitle
+                collapsed={false}
+                wrapperStyles={{
+                    fontSize: "22px",
+                }}
+                />
+
+                <GoogleButton />
+
+                <Text alignItems="center" display="inherit" fontSize="xs" color="gray">
+                Powered by <GoogleIcon /> Auth0
+                </Text>
+            </VStack>
+            </Box>
+        );
+    <%_ } _%>
+
+    <%_ if (answers["ui-framework"] === "no") { _%>
+        return(
+            <div
+                style={{
+                    height: "100vh",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+            <GoogleButton />            
+            </div>
+        );
+    <%_ } _%>
 };

bf59f4690768ae530a02c51a10a3999d6a37c10b
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-03-29 16:30:48 +0300

    feat: redesing nextjs google

--- refine-nextjs/plugins/auth-provider-google/pages/login/index.tsx
+++ refine-nextjs/plugins/auth-provider-google/pages/login/index.tsx
@@ -1,14 +1,18 @@
-<%_ if (answers["ui-framework"] === 'antd') { _%>
-    import { Button } from "antd";
-<%_ } _%>
+<%_ if (answers["ui-framework"] === "antd") { _%>
+import { ThemedTitle } from "@refinedev/antd";
+import { Button, Typography, Layout, Space } from "antd";
+<%_ } _%> 
 <%_ if (answers["ui-framework"] === 'mui') { _%>
-    import { Button } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
+import { ThemedTitle } from "@refinedev/mui";
 <%_ } _%>
 <%_ if (answers["ui-framework"] === 'mantine') { _%>
-    import { Button } from "@mantine/core";
+import { Button, Box, Space, Text } from "@mantine/core";
+import { ThemedTitle } from "@refinedev/mantine";
 <%_ } _%>
 <%_ if (answers["ui-framework"] === 'chakra') { _%>
-    import { Button } from "@chakra-ui/react";
+import { Button, Box, Text, VStack } from "@chakra-ui/react";
+import { ThemedTitle } from "@refinedev/chakra-ui";
 <%_ } _%>
 import { useLogin } from "@refinedev/core";
     
@@ -34,34 +38,189 @@ export default function Login() {
     const t = useTranslate();
     <%_ } _%>
 
-    return(
-        <div
+    const GoogleIcon = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="13"
+      height="13"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8"></path>
+    </svg>
+  );
+
+    <%_ if (answers["ui-framework"] === "antd") { _%>
+        return (
+            <Layout
+            style={{
+                height: "100vh",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+            <Space direction="vertical" align="center" size="large">
+                <ThemedTitle
+                collapsed={false}
+                wrapperStyles={{
+                    fontSize: "22px",
+                }}
+                />
+                <Button
+                    type="primary"
+                    size="middle"
+                    onClick={() => login({})}
+                >
+                    <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
+                    {t("pages.login.signin", "Sign in")}
+                    <%_ } else { _%>
+                        Sign in
+                    <%_ } _%>
+                </Button>
+                <Typography.Text type="secondary">
+                Powered by <GoogleIcon /> Google
+                </Typography.Text>
+            </Space>
+            </Layout>
+        );
+    <%_ } _%> 
+    
+    <%_ if (answers["ui-framework"] === 'mui') { _%>
+        return (
+            <Container
             style={{
                 height: "100vh",
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
             }}
-        >
-            <%_ if (answers["ui-framework"] === "no") { _%>
-                <button onClick={() => login({})}>
+            >
+            <Box
+                display="flex"
+                gap="25px"
+                justifyContent="center"
+                flexDirection="column"
+            >
+                <ThemedTitle
+                collapsed={false}
+                wrapperStyles={{
+                    fontSize: "22px",
+                }}
+                />
+
+                <Button variant="contained" onClick={() => login({})}>
                     <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
                     {t("pages.login.signin", "Sign in")}
                     <%_ } else { _%>
-                        Sign in with Auth0
+                        Sign in
                     <%_ } _%>
-            </button>
-            <%_ } else { _%>
-                <Button onClick={() => login({})}>
+                </Button>
+                <Typography align="center" color={"text.secondary"} fontSize="14px">
+                Powered by <GoogleIcon /> Google
+                </Typography>
+            </Box>
+            </Container>
+        );
+    <%_ } _%>
+
+    <%_ if (answers["ui-framework"] === 'mantine') { _%>
+        return (
+            <Box
+            sx={{
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+            <ThemedTitle
+                collapsed={false}
+                wrapperStyles={{
+                fontSize: "22px",
+                }}
+            />
+            <Space h="xl" />
+
+            <Button
+                type="button"
+                variant="filled"
+                onClick={() => login({})}
+            >
+                <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
+                    {t("pages.login.signin", "Sign in")}
+                <%_ } else { _%>
+                    Sign in
+                <%_ } _%>
+            </Button>
+            <Space h="lg" />
+            <Text fz="sm" color="gray">
+                Powered by <GoogleIcon /> Google
+            </Text>
+            </Box>
+        );
+    <%_ } _%>
+
+    <%_ if (answers["ui-framework"] === 'chakra') { _%>
+        return (
+            <Box
+            sx={{
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+            <VStack spacing="8" align="stretch">
+                <ThemedTitle
+                collapsed={false}
+                wrapperStyles={{
+                    fontSize: "22px",
+                }}
+                />
+
+                <Button colorScheme="blue" onClick={() => login({})}>
+                    <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
+                    {t("pages.login.signin", "Sign in")}
+                    <%_ } else { _%>
+                        Sign in
+                    <%_ } _%>
+                </Button>
+
+                <Text alignItems="center" display="inherit" fontSize="xs" color="gray">
+                Powered by <GoogleIcon /> Google
+                </Text>
+            </VStack>
+            </Box>
+        );
+    <%_ } _%>
+
+    <%_ if (answers["ui-framework"] === "no") { _%>
+        return(
+            <div
+                style={{
+                    height: "100vh",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+            <button onClick={() => login({})}>
                 <%_ if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
                 {t("pages.login.signin", "Sign in")}
                 <%_ } else { _%>
-                Sign in
+                    Sign in
                 <%_ } _%>
-            </Button>
-            <%_ } _%>                
-        </div>
-    );
+            </button>               
+            </div>
+        );
+    <%_ } _%>
 }
 
 Login.noLayout = true;

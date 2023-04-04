1ead90b9ab3b4b4fb89ed60e9e8aa4f56413286b
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-04-04 15:34:57 +0300

    feat: add remix

---
+++ refine-remix/plugins/data-provider-appwrite/app/routes/_auth.register.tsx
@@ -0,0 +1,21 @@
+<%_ if (answers["ui-framework"] === 'antd') { _%>
+    import { AuthPage } from "@refinedev/antd";
+ <%_ } _%>
+ <%_ if (answers["ui-framework"] === 'mui') { _%>
+     import { AuthPage } from "@refinedev/mui";
+ <%_ } _%>
+ <%_ if (answers["ui-framework"] === 'mantine') { _%>
+     import { AuthPage } from "@refinedev/mantine";
+ <%_ } _%>
+ <%_ if (answers["ui-framework"] === 'chakra') { _%>
+     import { AuthPage } from "@refinedev/chakra-ui";
+ <%_ } _%>
+ <%_ if (answers[`ui-framework`] === "no") { _%>
+    import { AuthPage } from "@refinedev/core";
+<%_ } _%>
+
+export default function Register() {
+    return (
+        <AuthPage type="register" />
+    );
+}

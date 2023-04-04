7ce91840220ba2bc1f8404c3098015e154911014
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-04-04 15:42:42 +0300

    feat: custom auth provider for remix

---
+++ refine-remix/plugins/auth-provider-custom/app/routes/_auth.forgot-password.tsx
@@ -0,0 +1,56 @@
+<%_ if (answers["ui-framework"] === 'antd') { _%>
+import {
+    AuthPage,
+    <%_ if (selectedSvg || selectedTitle) { _%>
+    ThemedTitle,
+    <%_ } _%>
+} from "@refinedev/antd";
+<%_ } _%>
+<%_ if (answers["ui-framework"] === 'mui') { _%>
+import {
+    AuthPage,
+    <%_ if (selectedSvg || selectedTitle) { _%>
+    ThemedTitle,
+    <%_ } _%>
+} from "@refinedev/mui";
+<%_ } _%>
+<%_ if (answers["ui-framework"] === 'mantine') { _%>
+import {
+    AuthPage,
+    <%_ if (selectedSvg || selectedTitle) { _%>
+    ThemedTitle,
+    <%_ } _%>
+} from "@refinedev/mantine";
+<%_ } _%>
+<%_ if (answers["ui-framework"] === 'chakra') { _%>
+import {
+    AuthPage,
+    <%_ if (selectedSvg || selectedTitle) { _%>
+    ThemedTitle,
+    <%_ } _%>
+} from "@refinedev/chakra-ui";
+<%_ } _%>
+<%_ if (selectedSvg && answers["ui-framework"] !== "no" ) { _%>
+import { AppIcon } from "app/components/app-icon";
+<%_ } _%>
+
+export default function ForgotPassword() {
+    return (
+        <AuthPage
+            type="forgotPassword"
+            <%_ if ((selectedSvg || selectedTitle) && answers["ui-framework"] !== "no") { _%>
+            title={(
+                <ThemedTitle
+                    collapsed={false}
+                    <%_ if (selectedTitle) { _%>
+                        text="<%= selectedTitle %>"
+                    <%_ } _%>
+                    <%_ if (selectedSvg) { _%>
+                        icon={<AppIcon />}
+                    <%_ } _%>
+                />
+            )}
+            <%_ } _%>
+        />
+    );
+}

9c1cdde6fb64869ef33ef81ea0b07f51cb0058a2
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-04-06 18:28:47 +0300

    feat: updated e2e specs

--- cypress/e2e/build-test.cy.js
+++ cypress/e2e/build-test.cy.js
@@ -5,6 +5,11 @@ Cypress.on("uncaught:exception", (err, runnable) => {
 });
 
 describe("build test", () => {
+    beforeEach(() => {
+        cy.clearAllCookies();
+        cy.clearAllLocalStorage();
+    });
+
     it("should build", () => {
         cy.visit("http://localhost:3000");
 
@@ -21,13 +26,50 @@ describe("build test", () => {
         } else {
             cy.contains("refine Project", { timeout: 10000 }).should("exist");
 
+            cy.contains("Forgot password?").click();
+
+            cy.url().should("contain", "/forgot-password");
+
+            cy.contains("Sign in").click();
+
+            cy.contains("Sign up").click();
+
+            cy.url().should("contain", "/register");
+
+            cy.contains("Sign up for your account").should("exist");
+
+            cy.contains("Sign in").click();
+
+            cy.visit("http://localhost:3000/i-dont-exist");
+
+            cy.url().should(
+                "eq",
+                "http://localhost:3000/login?to=%2Fi-dont-exist",
+            );
+
+            cy.contains("Sign in to your account").should("exist");
+
             cy.get("form").submit();
 
-            cy.contains("Blog Posts", { timeout: 10000 }).should("exist");
+            cy.url().should("eq", "http://localhost:3000/i-dont-exist", {
+                timeout: 3000,
+            });
+
+            cy.contains("Sorry, the page you visited does not exist.").should(
+                "exist",
+            );
+
+            cy.contains("Back Home").click();
+
+            cy.contains("Blog Posts").should("exist");
 
             cy.contains("Categories").should("exist");
 
             cy.contains("Logout").should("exist");
+
+            cy.contains("English").should("exist");
+
+            cy.contains("🔆").click();
         }
     });
 });

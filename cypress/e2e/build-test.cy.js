7295a40ab575cc7e2ccbdc8345641e4ee1f2dd0f
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-04-06 17:38:42 +0300

    feat: update e2e specs

--- cypress/e2e/build-test.cy.js
+++ cypress/e2e/build-test.cy.js
@@ -1,9 +1,33 @@
 /// <reference types="cypress" />
 
+Cypress.on("uncaught:exception", (err, runnable) => {
+    return false;
+});
+
 describe("build test", () => {
     it("should build", () => {
         cy.visit("http://localhost:3000");
 
-        cy.contains("refine", { timeout: 10000 }).should("exist");
+        if (
+            ["keycloak", "google", "auth0"].includes(
+                Cypress.env("AUTH_PROVIDER"),
+            )
+        ) {
+            cy.contains("refine Project", { timeout: 10000 }).should("exist");
+
+            cy.contains("Sign in").click();
+
+            cy.url().should("not.contain", "http://localhost:3000");
+        } else {
+            cy.contains("refine Project", { timeout: 10000 }).should("exist");
+
+            cy.get("form").submit();
+
+            cy.contains("Blog Posts", { timeout: 10000 }).should("exist");
+
+            cy.contains("Categories").should("exist");
+
+            cy.contains("Logout").should("exist");
+        }
     });
 });

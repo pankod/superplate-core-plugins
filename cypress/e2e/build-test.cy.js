8f8563f27db3c46f761fc5c2a65f4c43a689856b
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-04-06 16:10:24 +0300

    fix: update e2e spec

--- cypress/e2e/build-test.cy.js
+++ cypress/e2e/build-test.cy.js
@@ -4,6 +4,6 @@ describe("build test", () => {
     it("should build", () => {
         cy.visit("http://localhost:3000");
 
-        cy.contains("Refine", { timeout: 6000 }).should("exist");
+        cy.contains("refine Project", { timeout: 10000 }).should("exist");
     });
 });

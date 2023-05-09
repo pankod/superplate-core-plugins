d79f271655ccc461acee8c6b61dea64cfc7d56dc
Salih Özdemir <salihozdemir94@gmail.com>
2023-05-09 17:30:21 +0300

    fix(refine-nextjs): update the `next.-i18next.config` (#354)

--- refine-nextjs/plugins/i18n-mantine/next-i18next.config.js
+++ refine-nextjs/plugins/i18n-mantine/next-i18next.config.js
@@ -4,6 +4,6 @@ module.exports = {
     i18n: {
         defaultLocale: "en",
         locales: ["en", "de"],
-        localePath: path.resolve("./public/locales"),
     },
+    localePath: path.resolve("./public/locales"),
 };

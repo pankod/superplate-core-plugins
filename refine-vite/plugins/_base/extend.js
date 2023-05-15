953694b7a028dac9ccc1886924c345ef645d3d0b
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-05-15 13:33:26 +0300

    feat(vite): add hasura inferencer option

--- refine-vite/plugins/_base/extend.js
+++ refine-vite/plugins/_base/extend.js
@@ -139,7 +139,6 @@ module.exports = {
             if (
                 ![
                     "data-provider-graphql",
-                    "data-provider-hasura",
                     "data-provider-medusa",
                 ].includes(dataProvider) &&
                 base._app.hasRoutes === true

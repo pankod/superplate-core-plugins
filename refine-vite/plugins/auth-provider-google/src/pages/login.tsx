6de89a66c630659088906f047c8b4b31f364d8db
Ömer Faruk APLAK <omer@pankod.com>
2023-05-23 13:30:20 +0300

    chore: fix Box import for google auth

--- refine-vite/plugins/auth-provider-google/src/pages/login.tsx
+++ refine-vite/plugins/auth-provider-google/src/pages/login.tsx
@@ -7,6 +7,7 @@ import { Typography, Layout, Space } from "antd";
 <%_ } _%> 
 <%_ if (answers["ui-framework"] === 'mui') { _%>
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { ThemedTitleV2 } from "@refinedev/mui";

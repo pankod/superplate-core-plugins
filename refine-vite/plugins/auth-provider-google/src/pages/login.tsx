1501ca20969af6d1c5f0f1215f72044ba1ee70c0
Ali Emir Şen <senaliemir@gmail.com>
2023-05-23 11:13:57 +0300

    chore(mui-vite): update imports

--- refine-vite/plugins/auth-provider-google/src/pages/login.tsx
+++ refine-vite/plugins/auth-provider-google/src/pages/login.tsx
@@ -6,7 +6,9 @@ import { ThemedTitleV2 } from "@refinedev/antd";
 import { Typography, Layout, Space } from "antd";
 <%_ } _%> 
 <%_ if (answers["ui-framework"] === 'mui') { _%>
-import { Box, Container, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
 import { ThemedTitleV2 } from "@refinedev/mui";
 <%_ } _%>
 <%_ if (answers["ui-framework"] === 'mantine') { _%>

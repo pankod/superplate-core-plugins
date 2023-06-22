532be7810be0872e06335f5d369e63edfbea2252
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-06-22 11:19:38 +0300

    fix: add `languagedetector` for `chakra-ui` and `mantine`

--- refine-react/plugins/i18n-mantine/src/i18n.ts
+++ refine-react/plugins/i18n-mantine/src/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 import Backend from "i18next-xhr-backend";
 
 i18n.use(Backend)
+    .use(detector)
     .use(initReactI18next)
     .init({
         supportedLngs: ["en", "de"],

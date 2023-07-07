4fae938474eae7a479dd36175d699e8e4a1091b8
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2023-07-07 15:29:13 +0300

    fix: refine-nextjs auth google provider

--- refine-nextjs/plugins/auth-provider-google/pages/api/auth/[...nextauth].ts
+++ refine-nextjs/plugins/auth-provider-google/pages/api/auth/[...nextauth].ts
@@ -6,8 +6,8 @@ export const authOptions = {
     providers: [
         // !!! Should be stored in .env file.
         GoogleProvider({
-            clientId: `477775060025-dtg8bbi9fi2ff3o95b25gog1hlik9i6b.apps.googleusercontent.com`,
-            clientSecret: `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`,
+            clientId: `1041339102270-e1fpe2b6v6u1didfndh7jkjmpcashs4f.apps.googleusercontent.com`,
+            clientSecret: `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`,
         }),
     ],
     secret: `UItTuD1HcGXIj8ZfHUswhYdNd40Lc325R8VlxQPUoR0=`,

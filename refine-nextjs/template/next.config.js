6a6a765d1d9e4ebd7b425fa71fcd2febda0733a6
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-07-12 18:11:18 +0300

    fix(nextjs-antd): remove transpilePackages

--- refine-nextjs/template/next.config.js
+++ refine-nextjs/template/next.config.js
@@ -5,34 +5,12 @@
         i18n, experimental: {
             newNextLinkBehavior: true,
         },
-        transpilePackages: [
-            '@refinedev/antd',
-            "@refinedev/inferencer",
-            'antd',
-            '@ant-design/pro-components',
-            '@ant-design/pro-layout',
-            '@ant-design/pro-utils',
-            '@ant-design/pro-provider',
-            'rc-pagination',
-            'rc-picker'
-        ],
     };
 <%_ } else if (answers["ui-framework"] === "antd" && answers[`i18n-${answers["ui-framework"]}`] === "no") { _%>
     module.exports = {
         experimental: {
             newNextLinkBehavior: true,
         },
-        transpilePackages: [
-            '@refinedev/antd',
-            "@refinedev/inferencer",
-            'antd',
-            '@ant-design/pro-components',
-            '@ant-design/pro-layout',
-            '@ant-design/pro-utils',
-            '@ant-design/pro-provider',
-            'rc-pagination',
-            'rc-picker'
-        ],
     };
 <%_ } else if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
     const { i18n } = require("./next-i18next.config");

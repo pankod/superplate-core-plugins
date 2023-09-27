5f3e3d288f42ebc660aa55010cd7adecd4e6680d
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2023-09-27 14:30:30 +0300

    feat: add devtools to remix

--- refine-remix/template/app/root.tsx
+++ refine-remix/template/app/root.tsx
@@ -10,6 +10,7 @@ import {
 } from "@remix-run/react";
 
 import { Refine, GitHubBanner, <%- (_app.refineImports || []).join("\n,") _%> } from '@refinedev/core';
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 <%_ if (answers["ui-framework"] === 'antd') { _%>
     import { <%- (_app.refineAntdImports || []).join("\n,") _%> } from '@refinedev/antd';
 <%_ } _%>
@@ -59,61 +60,64 @@ export default function App() {
             <GitHubBanner />
             <RefineKbarProvider>
                 <%- top.join("\n") %>
-                    <Refine
-                        routerProvider={routerProvider}
-                        <%- (_app.refineProps || []).join("\n") %>
-                        <%_ if (answers["inferencer"] === 'inferencer' || answers["inferencer-headless"] === 'inferencer-headless') { _%>
-                        resources={[
-                            <%_ if (answers["data-provider"] === 'data-provider-strapi-v4') { _%>
-                            {
-                                name: "blog-posts",
-                                list: "/blog-posts",
-                                create: "/blog-posts/create",
-                                edit: "/blog-posts/edit/:id",
-                                show: "/blog-posts/show/:id",
-                                meta: {
-                                    canDelete: true,
+                    <DevtoolsProvider>
+                        <Refine
+                            routerProvider={routerProvider}
+                            <%- (_app.refineProps || []).join("\n") %>
+                            <%_ if (answers["inferencer"] === 'inferencer' || answers["inferencer-headless"] === 'inferencer-headless') { _%>
+                            resources={[
+                                <%_ if (answers["data-provider"] === 'data-provider-strapi-v4') { _%>
+                                {
+                                    name: "blog-posts",
+                                    list: "/blog-posts",
+                                    create: "/blog-posts/create",
+                                    edit: "/blog-posts/edit/:id",
+                                    show: "/blog-posts/show/:id",
+                                    meta: {
+                                        canDelete: true,
+                                    },
                                 },
-                            },
-                            <%_ } else { _%>
-                            {
-                                name: "blog_posts",
-                                list: "/blog-posts",
-                                create: "/blog-posts/create",
-                                edit: "/blog-posts/edit/:id",
-                                show: "/blog-posts/show/:id",
-                                meta: {
-                                    canDelete: true,
+                                <%_ } else { _%>
+                                {
+                                    name: "blog_posts",
+                                    list: "/blog-posts",
+                                    create: "/blog-posts/create",
+                                    edit: "/blog-posts/edit/:id",
+                                    show: "/blog-posts/show/:id",
+                                    meta: {
+                                        canDelete: true,
+                                    },
                                 },
-                            },
+                                <%_ } _%>
+                                {
+                                    name: "categories",
+                                    list: "/categories",
+                                    create: "/categories/create",
+                                    edit: "/categories/edit/:id",
+                                    show: "/categories/show/:id",
+                                    meta: {
+                                        canDelete: true,
+                                    },
+                                }
+                            ]}
                             <%_ } _%>
-                            {
-                                name: "categories",
-                                list: "/categories",
-                                create: "/categories/create",
-                                edit: "/categories/edit/:id",
-                                show: "/categories/show/:id",
-                                meta: {
-                                    canDelete: true,
-                                },
-                            }
-                        ]}
-                        <%_ } _%>
-                        options={{
-                            syncWithLocation: true,
-                            warnWhenUnsavedChanges: true,
-                            <%_ if (typeof projectId !== 'undefined' && projectId !== '') { _%>
-                                projectId: "<%= projectId %>",
-                            <%_ } _%>
-                            <%- (_app.refineOptions || []).join("\n") %>
-                        }}
-                    >
-                        <>
-                        <Outlet />
-                        <UnsavedChangesNotifier />
-                        <RefineKbar />
-                        </>
-                    </Refine>
+                            options={{
+                                syncWithLocation: true,
+                                warnWhenUnsavedChanges: true,
+                                <%_ if (typeof projectId !== 'undefined' && projectId !== '') { _%>
+                                    projectId: "<%= projectId %>",
+                                <%_ } _%>
+                                <%- (_app.refineOptions || []).join("\n") %>
+                            }}
+                        >
+                            <>
+                            <Outlet />
+                            <UnsavedChangesNotifier />
+                            <RefineKbar />
+                            </>
+                        </Refine>
+                        <DevtoolsPanel />
+                    </DevtoolsProvider>
                 <%- bottom.join("\n") %>
             </RefineKbarProvider>
             <ScrollRestoration />

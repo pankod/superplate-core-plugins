c31bd2d190ba0bd060d153ac996306ba0da70364
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-10-16 17:56:45 +0300

    feat: use new antd notification provider (#375)

--- refine-remix/plugins/antd/extend.js
+++ refine-remix/plugins/antd/extend.js
@@ -1,11 +1,15 @@
 const base = {
     _app: {
-        refineProps: ["notificationProvider={notificationProvider}"],
-        import: ['import resetStyle from "@refinedev/antd/dist/reset.css";'],
-        refineAntdImports: ["notificationProvider"],
+        refineProps: ["notificationProvider={useNotificationProvider}"],
+        import: [
+            `import { App as AntdApp } from "antd"`,
+            'import resetStyle from "@refinedev/antd/dist/reset.css";',
+        ],
+        refineAntdImports: ["useNotificationProvider"],
         styleImport: ['{ rel: "stylesheet", href: resetStyle }'],
         wrapper: [
             [`<ColorModeContextProvider>`, `</ColorModeContextProvider>`],
+            [`<AntdApp>`, `</AntdApp>`],
         ],
         localImport: [`import { ColorModeContextProvider } from "@contexts";`],
     },

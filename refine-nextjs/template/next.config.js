7db6757f714117f75061707d769e9f8eab112e35
Ali Emir Şen <senaliemir@gmail.com>
2023-10-20 11:18:23 +0300

    chore(refine-nextjs): remove redundant experimental config (#376)

--- refine-nextjs/template/next.config.js
+++ refine-nextjs/template/next.config.js
@@ -2,29 +2,17 @@
     const { i18n } = require("./next-i18next.config");
 
     module.exports = {
-        i18n, experimental: {
-            newNextLinkBehavior: true,
-        },
+        i18n,
     };
 <%_ } else if (answers["ui-framework"] === "antd" && answers[`i18n-${answers["ui-framework"]}`] === "no") { _%>
-    module.exports = {
-        experimental: {
-            newNextLinkBehavior: true,
-        },
-    };
+    module.exports = {};
 <%_ } else if (answers[`i18n-${answers["ui-framework"]}`] !== "no") { _%>
     const { i18n } = require("./next-i18next.config");
 
     module.exports = {
-        i18n, experimental: {
-            newNextLinkBehavior: true,
-        },
+        i18n,
     };
 <%_ } else { _%>
-    module.exports = {
-        experimental: {
-            newNextLinkBehavior: true,
-        },
-    };
+    module.exports = {};
 <%_ } _%>
 

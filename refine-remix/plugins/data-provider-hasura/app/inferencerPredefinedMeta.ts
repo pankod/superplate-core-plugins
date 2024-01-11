6165dd9bc9149f84a773057e63a79b56e74ff698
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-01-11 15:18:18 +0300

    feat(hasura): update hasura inferencer meta to use sql

--- refine-remix/plugins/data-provider-hasura/app/inferencerPredefinedMeta.ts
+++ refine-remix/plugins/data-provider-hasura/app/inferencerPredefinedMeta.ts
@@ -1,3 +1,5 @@
+import gql from "graphql-tag";
+
 /**
  * This `meta` object is used to define the necessary metadata for inferencer to work with.
  *
@@ -7,27 +9,194 @@
 export const inferencerPredefinedMeta = {
     blog_posts: {
         getList: {
-            fields: [
-                "id",
-                "title",
-                {
-                    category: ["id", "title"],
-                },
-                "category_id",
-                "content",
-                "created_at",
-            ],
+            gqlQuery: gql`
+                query BlogPostsList(
+                    $offset: Int!
+                    $limit: Int!
+                    $order_by: [blog_posts_order_by!]
+                    $where: blog_posts_bool_exp
+                ) {
+                    blog_posts(
+                        offset: $offset
+                        limit: $limit
+                        order_by: $order_by
+                        where: $where
+                    ) {
+                        id
+                        title
+                        content
+                        category_id
+                        created_at
+                        status
+                        category {
+                            id
+                            title
+                        }
+                    }
+                    blog_posts_aggregate(where: $where) {
+                        aggregate {
+                            count
+                        }
+                    }
+                }
+            `,
         },
         getOne: {
-            fields: ["id", "title", "content", "category_id"],
+            gqlQuery: gql`
+                query GetBlogPost($id: uuid!) {
+                    blog_posts_by_pk(id: $id) {
+                        id
+                        title
+                        content
+                        category_id
+                        created_at
+                        status
+                        category {
+                            id
+                            title
+                        }
+                    }
+                }
+            `,
+        },
+        create: {
+            foo: "bar",
+            objFoo: {
+                bar: "baz",
+            },
+            gqlMutation: gql`
+                mutation CreateBlogPosts($object: blog_posts_insert_input!) {
+                    insert_blog_posts_one(object: $object) {
+                        id
+                        title
+                        content
+                        created_at
+                        category_id
+                        status
+                        category {
+                            id
+                            title
+                        }
+                    }
+                }
+            `,
+        },
+        update: {
+            gqlMutation: gql`
+                mutation UpdateBlogPosts(
+                    $id: uuid!
+                    $object: blog_posts_set_input!
+                ) {
+                    update_blog_posts_by_pk(
+                        pk_columns: { id: $id }
+                        _set: $object
+                    ) {
+                        id
+                        title
+                        content
+                        created_at
+                        category_id
+                        status
+                        category {
+                            id
+                            title
+                        }
+                    }
+                }
+            `,
         },
     },
     categories: {
-        getList: {
-            fields: ["id", "title", "created_at"],
-        },
         default: {
-            fields: ["id", "title"],
+            gqlQuery: gql`
+                query CategoriesList(
+                    $offset: Int
+                    $limit: Int
+                    $order_by: [categories_order_by!]
+                    $where: categories_bool_exp
+                ) {
+                    categories(
+                        offset: $offset
+                        limit: $limit
+                        order_by: $order_by
+                        where: $where
+                    ) {
+                        id
+                        title
+                        created_at
+                    }
+                    categories_aggregate(where: $where) {
+                        aggregate {
+                            count
+                        }
+                    }
+                }
+            `,
+        },
+        getList: {
+            gqlQuery: gql`
+                query CategoriesList(
+                    $offset: Int
+                    $limit: Int
+                    $order_by: [categories_order_by!]
+                    $where: categories_bool_exp
+                ) {
+                    categories(
+                        offset: $offset
+                        limit: $limit
+                        order_by: $order_by
+                        where: $where
+                    ) {
+                        id
+                        title
+                        created_at
+                    }
+                    categories_aggregate(where: $where) {
+                        aggregate {
+                            count
+                        }
+                    }
+                }
+            `,
+        },
+        getOne: {
+            gqlQuery: gql`
+                query GetCategory($id: uuid!) {
+                    categories_by_pk(id: $id) {
+                        id
+                        title
+                        created_at
+                    }
+                }
+            `,
+        },
+        create: {
+            gqlMutation: gql`
+                mutation CreateCategory($object: categories_insert_input!) {
+                    insert_categories_one(object: $object) {
+                        id
+                        title
+                        created_at
+                    }
+                }
+            `,
+        },
+        update: {
+            gqlMutation: gql`
+                mutation UpdateCategory(
+                    $id: uuid!
+                    $object: categories_set_input!
+                ) {
+                    update_categories_by_pk(
+                        pk_columns: { id: $id }
+                        _set: $object
+                    ) {
+                        id
+                        title
+                        created_at
+                    }
+                }
+            `,
         },
     },
 };

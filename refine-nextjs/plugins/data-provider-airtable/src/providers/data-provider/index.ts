c8e4e29ff3b001a25409afd7668141868fccba83
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-03-26 15:10:58 +0300

    feat(data-provider-airtable): update api token

--- refine-nextjs/plugins/data-provider-airtable/src/providers/data-provider/index.ts
+++ refine-nextjs/plugins/data-provider-airtable/src/providers/data-provider/index.ts
@@ -2,7 +2,7 @@
 
 import airtable from "@refinedev/airtable";
 
-const API_TOKEN = "keyI18pnBeEMfPAIb";
+const API_TOKEN = "patI3quNRP17TNsjK.d59600d5955939ed02110fb1107036ff4482496004f020f5bf031f55789cd321";
 const BASE_ID = "appKYl1H4k9g73sBT";
 
 export const dataProvider = airtable(API_TOKEN, BASE_ID);

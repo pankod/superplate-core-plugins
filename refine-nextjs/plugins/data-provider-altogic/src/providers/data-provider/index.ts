88e61dab2e68a6af16a50130dbc916d36d2bc83c
Ömer Faruk APLAK <omer@pankod.com>
2024-04-17 11:17:26 +0300

    chore: remove altogic option

--- refine-nextjs/plugins/data-provider-altogic/src/providers/data-provider/index.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import airtable from "@refinedev/airtable";
-
-const API_TOKEN = "keyI18pnBeEMfPAIb";
-const BASE_ID = "appKYl1H4k9g73sBT";
-
-export const dataProvider = airtable(API_TOKEN, BASE_ID);

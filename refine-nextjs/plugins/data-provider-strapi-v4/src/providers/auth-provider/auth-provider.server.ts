96bef97c3b82098d511711b61b3b961e985a1ff0
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2024-05-30 15:49:36 +0300

    fix(nextjs-strapi-v4): auth flow

--- refine-nextjs/plugins/data-provider-strapi-v4/src/providers/auth-provider/auth-provider.server.ts
+++ refine-nextjs/plugins/data-provider-strapi-v4/src/providers/auth-provider/auth-provider.server.ts
@@ -1,22 +1,47 @@
-import { AuthBindings } from "@refinedev/core";
-import { TOKEN_KEY } from "@utility/constants";
+import { AuthProvider } from "@refinedev/core";
+import { API_URL, TOKEN_KEY } from "@utility/constants";
 import { cookies } from "next/headers";
 
-export const authProviderServer: Pick<AuthBindings, "check"> = {
+export const authProviderServer: Pick<AuthProvider, "check"> = {
     check: async () => {
         const cookieStore = cookies();
-        const auth = cookieStore.get(TOKEN_KEY);
+        const token = cookieStore.get(TOKEN_KEY);
+        const url = new URL("/api/users/me", API_URL);
 
-        if (auth) {
+        if (!token) {
             return {
-                authenticated: true,
+                authenticated: false,
+                redirectTo: "/login",
             };
         }
 
-        return {
-            authenticated: false,
-            logout: true,
-            redirectTo: "/login",
-        };
+        try {
+            const response = await fetch(url.href, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token.value}`,
+                },
+                cache: "no-cache",
+            });
+            const data = await response.json();
+            if (data.error || !response.ok) {
+                return {
+                    authenticated: false,
+                    redirectTo: "/login",
+                    error: data.error,
+                };
+            }
+
+            return {
+                authenticated: true,
+            };
+        } catch (error) {
+            return {
+                authenticated: false,
+                redirectTo: "/login",
+                error: error,
+            };
+        }
     },
 };

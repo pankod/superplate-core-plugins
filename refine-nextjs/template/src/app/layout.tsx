4b8423b9cc4c4a27cd8ad7dfc0f38e66bce37864
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2025-09-03 19:06:04 +0300

    fix: convert layouts to async function

--- refine-nextjs/template/src/app/layout.tsx
+++ refine-nextjs/template/src/app/layout.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
     },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;

30f5fec101e90e6d92efe0cb38798d9506058a6e
erenkurnaz <h.erenkurnaz@icloud.com>
2026-01-02 13:38:28 +0300

    refactor(supabase): add environment variable handling and remove constants file

--- refine-vite/plugins/data-provider-supabase/extend.js
+++ refine-vite/plugins/data-provider-supabase/extend.js
@@ -17,6 +17,18 @@ const base = {
         refineMuiImports: [],
         wrapper: [],
     },
+    _env: {
+        variables: [
+            "VITE_API_URL=https://iwdfzvfqbtokqetmbmbp.supabase.co",
+            "VITE_SUPABASE_API_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU",
+        ],
+    },
+    _constants: {
+        export: [
+            'export const SUPABASE_URL = import.meta.env.VITE_API_URL || "https://iwdfzvfqbtokqetmbmbp.supabase.co";',
+            'export const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_API_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";',
+        ],
+    },
 };
 module.exports = {
     extend() {

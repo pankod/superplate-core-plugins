f5bade45c6bf758408ec8f69e251dbcf5056b275
erenkurnaz <h.erenkurnaz@icloud.com>
2026-01-02 13:37:32 +0300

    refactor(extend): add environment variable handling for API_URL

--- refine-vite/plugins/data-provider-custom-json-rest/extend.js
+++ refine-vite/plugins/data-provider-custom-json-rest/extend.js
@@ -3,6 +3,9 @@ const base = {
         localImport: [`import { dataProvider } from "./providers/data";`],
         refineProps: [`dataProvider={dataProvider}`],
     },
+    _env: {
+        variables: ["VITE_API_URL=https://api.fake-rest.refine.dev"],
+    },
     _constants: {
         export: [
             'export const API_URL = import.meta.env.VITE_API_URL || "https://api.fake-rest.refine.dev";',
